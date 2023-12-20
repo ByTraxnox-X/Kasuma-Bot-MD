386ada95863e6659d165671190e6df01a25079d3
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-20 21:56:22 +0100

    KASUMA

--- plugins/main-apistatus.js
+++ plugins/main-apistatus.js
@@ -15,7 +15,7 @@ const handler = async (m, { conn }) => {
 
       const mensaje = `\t\t*Estado de la API:*
       
-*API:* https://api-sebastian.zipponodes.xyz   
+*API:* https://visionaryapi.boxmine.xyz   
 *Uptime:* ${uptime}
 *Latencia:* ${latencia}
 *Total de Solicitudes:* ${totalRequests}
