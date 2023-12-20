e8fe9be7483f80ece91ee96893c9b5c5e0d4016f
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-20 20:14:09 +0100

    KASUMA

--- plugins/AI-bard.js
+++ plugins/AI-bard.js
@@ -8,12 +8,13 @@ const handler = async (m, { conn, text }) => {
   try {
     conn.sendPresenceUpdate('composing', m.chat);
 
-    const apiUrl = `${apivisionary}/api/bard?text=${encodeURIComponent(text)}`;
+    const apiUrl = `https://visionaryapi.boxmine.xyz/api/bard?text=${encodeURIComponent(text)}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     if (data.status && data.result) {
-      const respuestaApi = data.result;
+      const respuestaApi = JSON.parse(data.result).result;
+
       conn.reply(m.chat, respuestaApi, m);
     } else {
       throw 'No se pudo obtener una respuesta válida';
