83b1bea910f49c37742f7d3c1a644d6ac02dc638
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-20 03:47:38 +0100

    KASUMA

--- plugins/AI-bard.js
+++ plugins/AI-bard.js
@@ -1,3 +1,4 @@
+import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text }) => {
@@ -8,18 +9,20 @@ const handler = async (m, { conn, text }) => {
   try {
     conn.sendPresenceUpdate('composing', m.chat);
 
-    const apiUrl = `https://vihangayt.me/tools/bard?q=${encodeURIComponent(text)}`;
+    const apiUrl = `https://apiruulzz.my.id/api/tools/gbard?query=${encodeURIComponent(text)}&apikey=dca5f647`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    if (data.status && data.data) {
-      const respuestaApi = data.data;
-      conn.reply(m.chat, respuestaApi, m);
+    if (data.status && data.result) {
+      const respuestaApi = data.result;
+      const translatedText = await translate(respuestaApi, { to: 'es' });
+
+      conn.reply(m.chat, translatedText.text, m);
     } else {
       throw 'No se pudo obtener una respuesta válida';
     }
   } catch (error) {
-    throw `Ocurrió un error`;
+    throw `Ocurrió un error: ${error}`;
   }
 };
 
