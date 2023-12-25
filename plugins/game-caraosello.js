5bc573e254952107847d59dd7b3bb5d3fd636fb0
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-25 17:08:56 +0100

    KASUMA

--- plugins/game-caraosello.js
+++ plugins/game-caraosello.js
@@ -4,58 +4,58 @@ let handler = async (m, { conn, text, command, usedPrefix, args }) => {
    let textos = `\t*CARA O SELLO*\n\nPuedes Jugar usando los comandos:\n\n${usedPrefix + command} cara\n${usedPrefix + command} cruz`;
 
    if (args[0] && ['cara', 'cruz'].includes(args[0].toLowerCase())) {
-       text = args[0].toLowerCase();
+      text = args[0].toLowerCase();
    } else {
-       conn.sendMessage(m.chat, { image: { url: pp }, caption: textos, mentions: [m.sender] }, { quoted: m });
-       return; 
+      conn.sendMessage(m.chat, { image: { url: pp }, caption: textos, mentions: [m.sender] }, { quoted: m });
+      return;
    }
 
    var pvjuegocs = Math.random();
    if (pvjuegocs < 0.50) {
-       pvjuegocs = 'cara';
+      pvjuegocs = 'cara';
    } else {
-       pvjuegocs = 'cruz';
+      pvjuegocs = 'cruz';
    }
 
    if (text == pvjuegocs) {
-       let who;
-       if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
-       else who = m.sender;
-       let name = conn.getName(who);
-       let dolares = global.db.data.users[m.sender].dolares += Math.floor(Math.random() * 101); // Premio entre 0 y 100 dólares
-       m.reply(`\tGanaste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Premio:* ${[dolares].getRandom()} $`);
+      let who;
+      if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
+      else who = m.sender;
+      let name = conn.getName(who);
+      let dolares = global.db.data.users[m.sender].dolares += Math.floor(Math.random() * 101); // Premio entre 0 y 100 dólares
+      m.reply(`\tGanaste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Premio:* ${[dolares].getRandom()} $`);
    } else if (text == 'cara') {
-       if (pvjuegocs == 'cara') {
-           let who;
-           if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
-           else who = m.sender;
-           let name = conn.getName(who);
-           let dolares = global.db.data.users[m.sender].dolares += Math.floor(Math.random() * 101);
-           m.reply(`\tGanaste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Premio:* ${[dolares].getRandom()} $`);
-       } else {
-           let who;
-           if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
-           else who = m.sender;
-           let name = conn.getName(who);
-           let dolares = global.db.data.users[m.sender].dolares -= 300;
-           m.reply(`\tPerdiste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Perdiste:* ${[dolares].getRandom()} $`);
-       }
+      if (pvjuegocs == 'cara') {
+         let who;
+         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
+         else who = m.sender;
+         let name = conn.getName(who);
+         let dolares = global.db.data.users[m.sender].dolares += Math.floor(Math.random() * 101);
+         m.reply(`\tGanaste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Premio:* ${[dolares].getRandom()} $`);
+      } else {
+         let who;
+         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
+         else who = m.sender;
+         let name = conn.getName(who);
+         let dolares = global.db.data.users[m.sender].dolares -= 300;
+         m.reply(`\tPerdiste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Perdiste:* ${[dolares].getRandom()} $`);
+      }
    } else if (text == 'cruz') {
-       if (pvjuegocs == 'cruz') {
-           let who;
-           if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
-           else who = m.sender;
-           let name = conn.getName(who);
-           let dolares = global.db.data.users[m.sender].dolares += Math.floor(Math.random() * 101);
-           m.reply(`\tGanaste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Premio:* ${[dolares].getRandom()} $`);
-       } else {
-           let who;
-           if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
-           else who = m.sender;
-           let name = conn.getName(who);
-           let dolares = global.db.data.users[m.sender].dolares -= 300;
-           m.reply(`\tPerdiste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Perdiste:* ${[dolares].getRandom()} $`);
-       }
+      if (pvjuegocs == 'cruz') {
+         let who;
+         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
+         else who = m.sender;
+         let name = conn.getName(who);
+         let dolares = global.db.data.users[m.sender].dolares += Math.floor(Math.random() * 101);
+         m.reply(`\tGanaste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Premio:* ${[dolares].getRandom()} $`);
+      } else {
+         let who;
+         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
+         else who = m.sender;
+         let name = conn.getName(who);
+         let dolares = global.db.data.users[m.sender].dolares -= 300;
+         m.reply(`\tPerdiste\n*Elegiste:* ${text}\n*Resultado:* ${pvjuegocs}\n*Perdiste:* ${[dolares].getRandom()} $`);
+      }
    }
 
    global.db.data.users[m.sender].wait = new Date() * 1;
