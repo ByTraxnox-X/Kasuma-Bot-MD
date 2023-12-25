1cc4087a5d3057d5dc91194cba516ee7ea277d51
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2023-12-25 15:00:15 -0500

    Actualizar keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,3 +1,16 @@
+from flask import Flask,render_template
+from threading import Thread
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Alive"
+def run():
+  app.run(host='0.0.0.0',port=8080)
+def keep_alive():  
+    t = Thread(target=run)
+    t.start()
+
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.write("I'm alive");
