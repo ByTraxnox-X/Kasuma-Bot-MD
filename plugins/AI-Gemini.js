a63b52779cbbedd7a9ba0767db005d86d08bc896
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-26 15:09:15 +0100

    KASUMA

---
+++ plugins/AI-Gemini.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text }) => {
+  if (!text) {
+    throw 'Por favor, proporciona un texto';
+  }
+
+  try {
+    conn.sendPresenceUpdate('composing', m.chat);
+
+    const apiUrl = `${apivisionary}/api/gemini?text=${encodeURIComponent(text)}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.status && data.message) {
+      const respuestaApi = data.message;
+
+      conn.reply(m.chat, respuestaApi, m);
+    } else {
+      throw 'No se pudo obtener una respuesta válida';
+    }
+  } catch (error) {
+    throw `Ocurrió un error: ${error}`;
+  }
+};
+
+handler.help = ['gemini'];
+handler.tags = ['ai'];
+handler.command = /^gemini$/i;
+
+export default handler;
