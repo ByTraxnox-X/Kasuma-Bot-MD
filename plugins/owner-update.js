5a7e662997729d086c4fccf1bf051bfcd7075fa7
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-27 17:43:08 +0100

    KASUMA

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -3,12 +3,12 @@ import { execSync } from 'child_process';
 let handler = async (m, { conn, text }) => {
   try {
     if (global.conn.user.jid == conn.user.jid) {
-      let stdout = execSync('git pull --all' + (m.fromMe && text ? ' ' + text : ''));
+      let stdout = execSync('git reset --hard && git pull --all' + (m.fromMe && text ? ' ' + text : ''));
       conn.reply(m.chat, stdout.toString(), m);
     }
   } catch {
     try {
-      var update = execSync('git remote set-url origin https://github.com/ByTraxnox-X/KasumaBot-MD.git && git pull --all');
+      var update = execSync('git remote set-url origin https://github.com/ByTraxnox-X/KasumaBot-MD.git && git reset --hard && git pull --all');
       await m.reply(update.toString());
     } catch {
       await m.reply(`Lo siento, tuve un error al actualizar el bot, inténtalo nuevamente`);
