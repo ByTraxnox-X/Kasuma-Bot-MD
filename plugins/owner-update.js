d59753a649413e104d14eb7b34eb955b0bba3a60
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-27 17:41:12 +0100

    KASUMA

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,17 +1,24 @@
-import { execSync } from 'child_process'
+import { execSync } from 'child_process';
+
 let handler = async (m, { conn, text }) => {
-try {  
-if (global.conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-conn.reply(m.chat, stdout.toString(), m)}
-//} catch {
-var update = execSync('git remote set-url origin https://github.com/ByTraxnox-X/KasumaBot-MD.git && git pull')
-await m.reply(update.toString())
-} catch {
-await m.reply(`Lo siento, tuve un error al actualizar el bot, intente nuevamente`) 
-}}
-handler.help = ['update']
-handler.tags = ['owner'] 
-handler.command = /^update|actualizar$/i
-handler.rowner = true
-export default handler
+  try {
+    if (global.conn.user.jid == conn.user.jid) {
+      let stdout = execSync('git pull --all' + (m.fromMe && text ? ' ' + text : ''));
+      conn.reply(m.chat, stdout.toString(), m);
+    }
+  } catch {
+    try {
+      var update = execSync('git remote set-url origin https://github.com/ByTraxnox-X/KasumaBot-MD.git && git pull --all');
+      await m.reply(update.toString());
+    } catch {
+      await m.reply(`Lo siento, tuve un error al actualizar el bot, inténtalo nuevamente`);
+    }
+  }
+};
+
+handler.help = ['update'];
+handler.tags = ['owner'];
+handler.command = /^update|actualizar$/i;
+handler.rowner = true;
+
+export default handler;
