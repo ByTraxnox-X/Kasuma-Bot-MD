8ff207c7f02a0c49d9ef0aedd32b6a9ee715d3d7
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-27 01:03:55 +0100

    KASUMA

--- plugins/game-cancion.js
+++ plugins/game-cancion.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 const timeout = 30000;
 const poin = 1000;
-const handler = async (m, {conn, usedPrefix}) => {
+const handler = async (m, { conn, usedPrefix }) => {
   conn.adivina_la_cancion = conn.adivina_la_cancion ? conn.adivina_la_cancion : {};
   const id = m.chat;
   if (id in conn.adivina_la_cancion) {
@@ -11,7 +11,7 @@ const handler = async (m, {conn, usedPrefix}) => {
   }
   const res = await fetchJson(`https://raw.githubusercontent.com/ByTraxnox-X/Kasuma-Bot-MD/main/src/game/adivina_la_cancion.json`);
   const json = res[Math.floor(Math.random() * res.length)];
-  const caption = `ADIVINA EL TITULO DE LA CANCION\nTiempo: ${(timeout / 1000).toFixed(2)} segundos:\nEscribe *${usedPrefix}pista* Para obtener una pista\nPremio: ${poin} XP\n\nDEBES RESPONDER A ESTE MENSAJE CON LAS RESPUESTAS!!`.trim();
+  const caption = `ADIVINA EL TITULO DE LA CANCION\n\nTiempo: ${(timeout / 1000).toFixed(2)} segundos:\nEscribe *${usedPrefix}pista* Para obtener una pista\nPremio: ${poin} XP\n\nDEBES RESPONDER A ESTE MENSAJE CON LAS RESPUESTAS!!`.trim();
   conn.adivina_la_cancion[id] = [
     await m.reply(caption),
     json, poin,
@@ -20,7 +20,7 @@ const handler = async (m, {conn, usedPrefix}) => {
       delete conn.adivina_la_cancion[id];
     }, timeout),
   ];
-  const aa = await conn.sendMessage(m.chat, {audio: {url: json.link_song}, fileName: `error.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
+  const aa = await conn.sendMessage(m.chat, { audio: { url: json.link_song }, fileName: `error.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
   if (!aa) return conn.sendFile(m.chat, json.link_song, 'coba-lagi.mp3', '', m);
 };
 handler.help = ['adivinacancion'];
@@ -29,9 +29,9 @@ handler.command = /^adivinacancion|canción$/i;
 export default handler;
 async function fetchJson(url, options) {
   try {
-options ? options : {};
-const res = await axios({method: 'GET', url: url, headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}, ...options});
-return res.data;
+    options ? options : {};
+    const res = await axios({ method: 'GET', url: url, headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36' }, ...options });
+    return res.data;
   } catch (err) {
     return err;
   }
