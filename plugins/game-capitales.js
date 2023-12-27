3068e94d70d27848db04085a5a3a27b42b9fb5b3
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-27 17:51:35 +0100

    KASUMA

---
+++ plugins/game-capitales.js
@@ -0,0 +1,39 @@
+import fs from 'fs'
+
+let timeout = 15000
+let poin = 1000
+
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    let id = m.chat
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
+        throw false
+    }
+    let tekateki = JSON.parse(fs.readFileSync("./src/game/capitales.json"))
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+    let _clue = json.response
+    let clue = _clue.replace(/[A-Za-z]/g, '_')
+    let caption = `
+ⷮ *${json.pais}*
+
+*Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
+*Bono:* +${poin} Exp
+
+Responde con el nombre de la ciudad completo!
+`.trim()
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta descubrir la capital de otro pais.`, conn.tekateki[id][0])
+            delete conn.tekateki[id]
+        }, timeout)
+    ]
+}
+
+handler.help = ['capitalde']
+handler.tags = ['game']
+handler.command = /^(capitalde|capitales|capital|adivinalacapital)$/i
+
+export default handler
