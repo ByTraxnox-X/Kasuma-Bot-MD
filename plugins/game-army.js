65a0c95de3210d81fcb32b2dde3695d1982c2922
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-28 17:05:08 +0100

    KASUMA

--- plugins/game-army.js
+++ plugins/game-army.js
@@ -28,36 +28,41 @@ function joinTeam(userId, team) {
 }
 
 let handler = async (m, { conn }) => {
-  const userId = m.sender;
-  const input = m.text.trim().toLowerCase();
-
-  if (input === 'iniciarguerra') {
-    if (!warSessions.has(userId)) {
-      conn.reply(m.chat, '*Primero, únete a un equipo con !guerra join a o !guerra join b.*', m);
-      return;
+    const userId = m.sender;
+    const input = m.text.trim().toLowerCase();
+  
+    console.log('Input:', input);
+  
+    if (input === 'iniciarguerra') {
+      console.log('Iniciando guerra...');
+      if (!warSessions.has(userId)) {
+        console.log('Usuario no registrado en la guerra.');
+        conn.reply(m.chat, '*Primero, únete a un equipo con !guerra join a o !guerra join b.*', m);
+        return;
+      }
+  
+      const team = teamAssignments.get(userId);
+      const enemyTeam = team === 'a' ? 'b' : 'a';
+  
+      const userArmy = createArmy(5, 8); // Ejército del usuario con 5 unidades de fuerza 8
+      const enemyArmy = createArmy(5, 7); // Ejército enemigo con 5 unidades de fuerza 7
+  
+      warSessions.set(userId, { team, userArmy, enemyTeam, enemyArmy });
+  
+      conn.reply(m.chat, '*¡Prepárate para la batalla!*', m);
+  
+      printBattleStatus(conn, m, team, userArmy, enemyArmy);
+    } else if (input.startsWith('guerra join')) {
+      console.log('Intento unirse a la guerra...');
+      const team = input.split(' ')[2];
+      if (team === 'a' || team === 'b') {
+        joinTeam(userId, team);
+        conn.reply(m.chat, `*Te has unido al equipo ${team.toUpperCase()}.*`, m);
+      } else {
+        conn.reply(m.chat, '*Por favor, únete a un equipo válido: A o B.*', m);
+      }
     }
-
-    const team = teamAssignments.get(userId);
-    const enemyTeam = team === 'a' ? 'b' : 'a';
-
-    const userArmy = createArmy(5, 8); // Ejército del usuario con 5 unidades de fuerza 8
-    const enemyArmy = createArmy(5, 7); // Ejército enemigo con 5 unidades de fuerza 7
-
-    warSessions.set(userId, { team, userArmy, enemyTeam, enemyArmy });
-
-    conn.reply(m.chat, '*¡Prepárate para la batalla!*', m);
-
-    printBattleStatus(conn, m, team, userArmy, enemyArmy);
-  } else if (input.startsWith('guerra join')) {
-    const team = input.split(' ')[2];
-    if (team === 'a' || team === 'b') {
-      joinTeam(userId, team);
-      conn.reply(m.chat, `*Te has unido al equipo ${team.toUpperCase()}.*`, m);
-    } else {
-      conn.reply(m.chat, '*Por favor, únete a un equipo válido: A o B.*', m);
-    }
-  }
-};
+  };
 
 handler.help = ['guerra', 'iniciarguerra', 'join'];
 handler.tags = ['game'];
