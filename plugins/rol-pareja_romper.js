d49dd0696dee00f8840946f1bd5e098f7cc5225b
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2023-12-28 13:29:10 -0500

    Actualizar plugins

---
+++ plugins/rol-pareja_romper.js
@@ -0,0 +1,30 @@
+let handler = async (m, { conn }) => { 
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = await conn.getName(who)
+let romper = global.db.data.users[m.sender].pasangan
+var ayg = global.db.data.users[m.sender]
+var beb = global.db.data.users[global.db.data.users[m.sender].pasangan]
+
+if(ayg.pasangan == ""){
+return await conn.reply(m.chat, `Usted *${name}* No tiene pareja`, m,  m)    
+}
+      
+if (typeof beb == "undefined"){
+await conn.reply(m.chat, `${name}* 💔 Rampio definitivamente con *${await conn.getName(romper)}*\n✩ Wa.me/${global.db.data.users[m.sender].pasangan.split('@')[0]}*\n\n${wm}`, m, m, { contextInfo: { mentionedJid: [ m.sender, romper ]}}) 
+ayg.pasangan = ""
+}
+
+if (m.sender == beb.pasangan){
+await conn.reply(m.chat, `*${name}* 💔 Rompio definitivamente con *${await conn.getName(romper)}*\n✩ Wa.me/${global.db.data.users[m.sender].pasangan.split('@')[0]}\n\n${wm}`, m, m, { contextInfo: { mentionedJid: [ m.sender, romper ]}}) 
+ayg.pasangan = ""
+beb.pasangan = ""
+}else {
+await conn.reply(m.chat, `Usted *${name}* No tiene pareja`, m,  m)    
+}}
+
+handler.help = ['romper'];
+handler.tags = ['rol'];
+handler.command = /^(cortar|romper|finish|terminar)$/i
+handler.group = true
+
+export default handler
