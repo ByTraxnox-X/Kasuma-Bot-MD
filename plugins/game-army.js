d412c3581484e54b24bbb30820eef6236557369f
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-28 17:13:38 +0100

    KASUMA

--- plugins/game-army.js
+++ plugins/game-army.js
@@ -1,23 +1,11 @@
 const warSessions = new Map();
 const teamAssignments = new Map();
 
-function createArmy(units, strength) {
-  const army = [];
-  for (let i = 0; i < units; i++) {
-    army.push(strength);
-  }
-  return army;
-}
-
-function printBattleStatus(conn, m, team, userArmy, enemyArmy) {
-  let result = `*Estado de la Batalla*\n\nTu equipo (${team.toUpperCase()}): ${userArmy.join(' ')}\nEquipo Enemigo: ${enemyArmy.join(' ')}`;
-  conn.reply(m.chat, result, m);
-}
-
 function joinTeam(userId, team, conn, m) {
   if (!warSessions.has(userId)) {
     warSessions.set(userId, { team });
     teamAssignments.set(userId, team);
+    conn.reply(m.chat, `*Te has unido al equipo ${team.toUpperCase()}.*`, m);
   } else {
     conn.reply(m.chat, '*Ya estás en la guerra.*', m);
   }
@@ -29,31 +17,11 @@ let handler = async (m, { conn }) => {
 
   console.log('Input:', input);
 
-  if (input === 'iniciarguerra') {
-    console.log('Iniciando guerra...');
-    if (!warSessions.has(userId)) {
-      console.log('Usuario no registrado en la guerra.');
-      conn.reply(m.chat, '*Primero, únete a un equipo con !guerra join a o !guerra join b.*', m);
-      return;
-    }
-
-    const team = teamAssignments.get(userId);
-    const enemyTeam = team === 'a' ? 'b' : 'a';
-
-    const userArmy = createArmy(5, 8);
-    const enemyArmy = createArmy(5, 7);
-
-    warSessions.set(userId, { team, userArmy, enemyTeam, enemyArmy });
-
-    conn.reply(m.chat, '*¡Prepárate para la batalla!*', m);
-
-    printBattleStatus(conn, m, team, userArmy, enemyArmy);
-  } else if (input.startsWith('guerra join')) {
+  if (input.startsWith('guerra join')) {
     console.log('Intento unirse a la guerra...');
     const team = input.split(' ')[2];
     if (team === 'a' || team === 'b') {
       joinTeam(userId, team, conn, m);
-      conn.reply(m.chat, `*Te has unido al equipo ${team.toUpperCase()}.*`, m);
     } else {
       conn.reply(m.chat, '*Por favor, únete a un equipo válido: A o B.*', m);
     }
