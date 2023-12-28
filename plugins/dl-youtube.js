26378ef9f9b04ea031b400dd1d149a7fc3edc6c2
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-28 03:43:49 +0100

    KASUMA

--- plugins/dl-youtube.js
+++ plugins/dl-youtube.js
@@ -1,44 +1,50 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { text }) => {
-  if (!text) throw `Ingrese el nombre de la canción.`;
+  if (!text) throw `Ingrese la búsqueda de YouTube. Por ejemplo: youtube [texto de búsqueda] [audio/video]`;
 
-  try {
-    const encodedText = encodeURIComponent(text);
-    const res = await fetch(`https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`);
-    const data = await res.json();
+  const isYouTubeCommand = /^youtube\s/i.test(text);
 
-    if (!data.resultado) {
-      throw 'No se encontraron resultados para la búsqueda.';
-    }
-
-    const { title, channel, description, seconds, image, download } = data.resultado;
-
-    const infoMessage = `*${title}*\n\n*Canal:* ${channel}\n*Descripción:* ${description}\n*Duración:* ${seconds} segundos`;
-
-    await conn.sendMessage(m.chat, { text: infoMessage.trim() }, { quoted: m });
-
-
-    const optionsMessage = '¿Quieres descargar el audio o el video?\n1. Audio\n2. Video';
-    await conn.sendMessage(m.chat, { text: optionsMessage }, { quoted: m });
-
-    const response = await conn.waitForMessage(m.chat, {
-      fromMe: true,
-      content: /^(1|2)$/i,
-    });
-
-    const userChoice = response.content.trim();
-
-    const fileURL = userChoice === '1' ? download.audio : download.video;
-    const fileBuffer = await (await fetch(fileURL)).buffer();
-
-    const fileType = userChoice === '1' ? 'audio/mpeg' : 'video/mp4';
-
-    await conn.sendMessage(m.chat, { [userChoice === '1' ? 'audio' : 'video']: fileBuffer, fileName: `${title}.${userChoice === '1' ? 'mp3' : 'mp4'}`, mimetype: fileType }, { quoted: m });
-  } catch (error) {
-    console.error(error);
-    throw 'Error al procesar la solicitud.';
+  if (!isYouTubeCommand) {
+    throw `El comando debe comenzar con "youtube". Por ejemplo: youtube [texto de búsqueda] [audio/video]`;
   }
+
+  const searchText = text.replace(/^youtube\s+/i, '').trim();
+
+  if (!searchText) {
+    throw 'Por favor, especifique el texto de búsqueda después de "youtube".';
+  }
+
+  const optionsMessage = '¿Quieres descargar el audio o el video?\n1. Audio\n2. Video';
+  await conn.sendMessage(m.chat, { text: optionsMessage }, { quoted: m });
+
+  const response = await conn.waitForMessage(m.chat, {
+    fromMe: true,
+    content: /^(1|2)$/i,
+  });
+
+
+  const userChoice = response.content.trim();
+
+  const encodedText = encodeURIComponent(searchText);
+  const res = await fetch(`https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`);
+  const data = await res.json();
+
+  if (!data.resultado) {
+    throw 'No se encontraron resultados para la búsqueda de YouTube.';
+  }
+
+  const { title, channel, description, seconds, download } = data.resultado;
+
+  const fileType = userChoice === '1' ? 'audio/mpeg' : 'video/mp4';
+  const fileURL = userChoice === '1' ? download.audio : download.video;
+  const fileBuffer = await (await fetch(fileURL)).buffer();
+
+  const infoMessage = `*${title}*\n\n*Canal:* ${channel}\n*Descripción:* ${description}\n*Duración:* ${seconds} segundos`;
+
+  await conn.sendMessage(m.chat, { text: infoMessage.trim() }, { quoted: m });
+
+  await conn.sendMessage(m.chat, { [userChoice === '1' ? 'audio' : 'video']: fileBuffer, fileName: `${title}.${userChoice === '1' ? 'mp3' : 'mp4'}`, mimetype: fileType }, { quoted: m });
 };
 
 handler.help = ['youtube'];
