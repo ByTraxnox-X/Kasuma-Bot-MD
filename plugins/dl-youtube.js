17edd332d3f16058b2a252a72327facbd3c71aba
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-28 04:10:19 +0100

    KASUMA

--- plugins/dl-youtube.js
+++ plugins/dl-youtube.js
@@ -1,52 +1,48 @@
+import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
     if (command == 'youtubeaudio') {
-        if (!text) throw `Por favor, ingresa el texto de búsqueda. Ejemplo: !youtubeaudio [texto de búsqueda]`;
-
-        const [, searchText] = text.match(/^!youtubeaudio\s+(.+)$/i);
-      
-        if (!searchText) throw 'Por favor, especifica el texto de búsqueda después de !youtubeaudio';
-      
-        const encodedText = encodeURIComponent(searchText);
-        const res = await fetch(`https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`);
-        const data = await res.json();
-      
-        if (!data.resultado) throw 'No se encontraron resultados para la búsqueda de YouTube.';
-      
-        const { title, channel, description, seconds, download } = data.resultado;
-        const fileBuffer = await (await fetch(download.audio)).buffer();
-        const infoMessage = `*${title}*\n\n*Canal:* ${channel}\n*Descripción:* ${description}\n*Duración:* ${seconds} segundos`;
-      
-        await conn.sendMessage(m.chat, { text: infoMessage.trim() }, { quoted: m });
-        await conn.sendMessage(m.chat, { audio: fileBuffer, fileName: `${title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
-      };
-
+            if (!text) throw `Ingrese el nombre de la canción.`;
+        
+            try {
+                const encodedText = encodeURIComponent(text);
+                const apiUrl = `https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`;
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                const audioDownloadLink = data.resultado.download.audio;
+                const audioResponse = await fetch(audioDownloadLink);
+                const audioBuffer = await audioResponse.buffer();
+        
+                await conn.sendMessage(m.chat, { audio: audioBuffer, fileName: `${text}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
+        
+            } catch (error) {
+                console.error(error);
+                throw 'Error, no hay resultados';
+            
+            }
+        }
       if (command == 'youtubevideo') {
-        if (!text) throw `Por favor, ingresa el texto de búsqueda. Ejemplo: !youtubevideo [texto de búsqueda]`;
+        if (!text) throw `Ingrese el nombre de la canción.`;
 
-        const [, searchText] = text.match(/^!youtubevideo\s+(.+)$/i);
-      
-        if (!searchText) throw 'Por favor, especifica el texto de búsqueda después de !youtubevideo';
-      
-        const encodedText = encodeURIComponent(searchText);
-        const res = await fetch(`https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`);
-        const data = await res.json();
-      
-        if (!data.resultado) throw 'No se encontraron resultados para la búsqueda de YouTube.';
-      
-        const { title, channel, description, seconds, download } = data.resultado;
-        const fileBuffer = await (await fetch(download.video)).buffer();
-      
-        const infoMessage = `*${title}*\n\n*Canal:* ${channel}\n*Descripción:* ${description}\n*Duración:* ${seconds} segundos`;
-
-        await conn.sendMessage(m.chat, { text: infoMessage.trim() }, { quoted: m });
-        await conn.sendMessage(m.chat, { video: fileBuffer, fileName: `${title}.mp4`, mimetype: 'video/mp4' }, { quoted: m });
-      };
+        try {
+            const encodedText = encodeURIComponent(text);
+            const apiUrl = `https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`;
+            const res = await fetch(apiUrl);
+            const data = await res.json();
+            const videoDownloadLink = data.resultado.download.video;
+            const videoResponse = await fetch(videoDownloadLink);
+            const videoBuffer = await videoResponse.buffer();
+    
+            await conn.sendMessage(m.chat, { video: videoBuffer, fileName: `${text}.mp4`, mimetype: 'video/mp4' }, { quoted: m });
+    
+        } catch (error) {
+            console.error(error);
+            throw 'Error, no hay resultados';
+        }
   }
+}
       
   handler.help = handler.command = ['youtubeaudio', 'youtubevideo']
   handler.tags = ['dl']
 
-  handler.owner = true
-  
   export default handler
