1b9eab0693ae4f8b422362a9b42a84e42085e7ac
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-28 03:49:39 +0100

    KASUMA

--- plugins/dl-youtube.js
+++ plugins/dl-youtube.js
@@ -1,31 +1,18 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { text }) => {
-  if (!text) throw `Ingrese la búsqueda de YouTube. Por ejemplo: youtube [texto de búsqueda] [audio/video]`;
-
-  const isYouTubeCommand = /^youtube\s/i.test(text);
-
-  if (!isYouTubeCommand) {
+  if (!text || !/^youtube\s/i.test(text)) {
     throw `El comando debe comenzar con "youtube". Por ejemplo: youtube [texto de búsqueda] [audio/video]`;
   }
 
-  const searchText = text.replace(/^youtube\s+/i, '').trim();
+  const [, searchText, fileType] = text.match(/^youtube\s+(.+?)(?:\s+(audio|video))?$/i);
 
   if (!searchText) {
-    throw 'Por favor, especifique el texto de búsqueda después de "youtube".';
+    throw 'Por favor, especifica el texto de búsqueda después de "youtube".';
   }
-
-  const optionsMessage = '¿Quieres descargar el audio o el video?\n1. Audio\n2. Video';
-  await conn.sendMessage(m.chat, { text: optionsMessage }, { quoted: m });
-
-  const response = await conn.waitForMessage(m.chat, {
-    fromMe: true,
-    content: /^(1|2)$/i,
-  });
-
-
-  const userChoice = response.content.trim();
-
+  if (!fileType) {
+    throw 'Por favor, especifica si quieres "audio" o "video" después de la búsqueda.';
+  }
   const encodedText = encodeURIComponent(searchText);
   const res = await fetch(`https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`);
   const data = await res.json();
@@ -36,15 +23,16 @@ const handler = async (m, { text }) => {
 
   const { title, channel, description, seconds, download } = data.resultado;
 
-  const fileType = userChoice === '1' ? 'audio/mpeg' : 'video/mp4';
-  const fileURL = userChoice === '1' ? download.audio : download.video;
+  const fileTypeLower = fileType.toLowerCase();
+
+  const fileURL = fileTypeLower === 'audio' ? download.audio : download.video;
   const fileBuffer = await (await fetch(fileURL)).buffer();
 
   const infoMessage = `*${title}*\n\n*Canal:* ${channel}\n*Descripción:* ${description}\n*Duración:* ${seconds} segundos`;
 
   await conn.sendMessage(m.chat, { text: infoMessage.trim() }, { quoted: m });
-
-  await conn.sendMessage(m.chat, { [userChoice === '1' ? 'audio' : 'video']: fileBuffer, fileName: `${title}.${userChoice === '1' ? 'mp3' : 'mp4'}`, mimetype: fileType }, { quoted: m });
+  
+  await conn.sendMessage(m.chat, { [fileTypeLower]: fileBuffer, fileName: `${title}.${fileTypeLower === 'audio' ? 'mp3' : 'mp4'}`, mimetype: fileTypeLower === 'audio' ? 'audio/mpeg' : 'video/mp4' }, { quoted: m });
 };
 
 handler.help = ['youtube'];
