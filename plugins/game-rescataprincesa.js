9aef53d219ab7e95abc3047400dc8685a3be55d3
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2023-12-28 21:15:10 -0500

    Actualizar game-rescataprincesa.js

--- plugins/game-rescataprincesa.js
+++ plugins/game-rescataprincesa.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedPrefix, text }) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
     let yesno = {
         'ayudar': 'Gracias por ayudarme',
         'dejarla': 'Dejastes que se la llevaran',
