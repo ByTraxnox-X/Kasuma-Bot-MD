7a0cd7e43fb62a90f14b08f4bfc7c8b4571ebcd5
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-29 20:00:00 +0100

    KASUMA

--- plugins/dl-youtube.js
+++ plugins/dl-youtube.js
@@ -1,48 +1,80 @@
 import fetch from 'node-fetch';
+let data;
+let buff;
+let mimeType;
+let fileName;
+let apiUrl;
+let enviando = false;
+const handler = async (m, { command, usedPrefix, conn, text }) => {
+  if (!text) throw `Ingrese el nombre o el enlace`;
+if (enviando) return;
+    enviando = true
+  try {
+    const apiUrls = [
+      `${apivisionary}/api/ytplay?text=${text}`,
+      `${visionary2}/api/ytplay?text=${text}`
+    ];
 
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-    if (command == 'youtubeaudio') {
-            if (!text) throw `Ingrese el nombre de la canción.`;
-        
-            try {
-                const encodedText = encodeURIComponent(text);
-                const apiUrl = `https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`;
-                const res = await fetch(apiUrl);
-                const data = await res.json();
-                const audioDownloadLink = data.resultado.download.audio;
-                const audioResponse = await fetch(audioDownloadLink);
-                const audioBuffer = await audioResponse.buffer();
-        
-                await conn.sendMessage(m.chat, { audio: audioBuffer, fileName: `${text}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
-        
-            } catch (error) {
-                console.error(error);
-                throw 'Error, no hay resultados';
-            
+    for (const url of apiUrls) {
+      try {
+        const res = await fetch(url);
+        data = await res.json();
+        if (data.resultado && data.resultado.url) {
+          break;
+        }
+      } catch {}
+    }
+
+    if (!data.resultado || !data.resultado.url) {
+      enviando = false;
+      throw `Error, intentelo de nuevo.`;
+    } else {
+      try {
+        if (command === 'play.1') {
+              apiUrl = `${visionary2}/api/v1/ytmp3?url=${data.resultado.url}`;
+              mimeType = 'audio/mpeg';
+              fileName = 'error.mp3';
+              buff = await conn.getFile(apiUrl);
+            } else if (command === 'play.2') {
+              apiUrl = `${visionary2}/api/v1/ytmp4?url=${data.resultado.url}`;
+              mimeType = 'video/mp4';
+              fileName = 'error.mp4';
+              buff = await conn.getFile(apiUrl);
+        }
+      } catch {
+          try {
+            if (command === 'play.1') {
+              apiUrl = `${visionary2}/api/v1/ytmp3?url=${data.resultado.url}`;
+              mimeType = 'audio/mpeg';
+              fileName = 'error.mp3';
+              buff = await conn.getFile(apiUrl);
+            } else if (command === 'play.2') {
+              apiUrl = `${visionary2}/api/v1/ytmp4?url=${data.resultado.url}`;
+              mimeType = 'video/mp4';
+              fileName = 'error.mp4';
+              buff = await conn.getFile(apiUrl);
             }
-        }
-      if (command == 'youtubevideo') {
-        if (!text) throw `Ingrese el nombre de la canción.`;
+          } catch {
+            enviando = false;
+            throw `Error, intentelo de nuevo.`;
+          }
+       }
+    }
 
-        try {
-            const encodedText = encodeURIComponent(text);
-            const apiUrl = `https://visionaryapi.boxmine.xyz/api/ytplay?text=${encodedText}`;
-            const res = await fetch(apiUrl);
-            const data = await res.json();
-            const videoDownloadLink = data.resultado.download.video;
-            const videoResponse = await fetch(videoDownloadLink);
-            const videoBuffer = await videoResponse.buffer();
-    
-            await conn.sendMessage(m.chat, { video: videoBuffer, fileName: `${text}.mp4`, mimetype: 'video/mp4' }, { quoted: m });
-    
-        } catch (error) {
-            console.error(error);
-            throw 'Error, no hay resultados';
-        }
+    const dataMessage = `*Título:* ${data.resultado.title}\n\n*Publicado:* ${data.resultado.publicDate}\n\n*Canal:* ${data.resultado.channel}\n\n*URL:* ${data.resultado.url}`;
+    await conn.sendMessage(m.chat, { text: dataMessage }, { quoted: m });
+
+    if (buff) {
+      await conn.sendMessage(m.chat, {[mimeType.startsWith('audio') ? 'audio' : 'video']: buff.data, mimetype: mimeType, fileName: fileName}, {quoted: m});
+      enviando = false;
+    } else {
+      enviando = false;
+      throw `Error, intentelo de nuevo.`;
+    }
+  } catch (error) {
+    enviando = false;
+    throw `Error, intentelo de nuevo.`;
   }
-}
-      
-  handler.help = handler.command = ['youtubeaudio', 'youtubevideo']
-  handler.tags = ['dl']
-
-  export default handler
+};
+handler.command = ['play.1', 'play.2'];
+export default handler;
