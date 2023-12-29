355db3b9f7352827e1cc23548052d1e77bfb66b0
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2023-12-28 21:13:46 -0500

    Actualizar plugins

---
+++ plugins/game-rescataprincesa.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, usedPrefix, text }) => {
+    let yesno = {
+        'ayudar': 'Gracias por ayudarme',
+        'dejarla': 'Dejastes que se la llevaran',
+    }[(args[0] || '')]
+    if (yesno === undefined)
+        throw `
+Hola, soy la princesa sebastian, *Ohhh noo*
+ayuda me estan secuestrando
+
+  *${usedPrefix + command} ayudar*
+  *${usedPrefix + command} dejarla*
+`.trim()
+ await conn.sendMessage(m.chat, isClose)
+
+
+}
+
+handler.help = ['rescatarprincesa'];
+handler.tags = ['game'];
+handler.command = /^rescatarprincesa$/i;
+
+export default handler;
