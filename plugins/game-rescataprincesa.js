5c9f6606f41496e03ed7a58d22ee8e837e03d2ad
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2023-12-28 21:48:11 -0500

    Actualizar game-rescataprincesa.js

--- plugins/game-rescataprincesa.js
+++ plugins/game-rescataprincesa.js
@@ -5,14 +5,31 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 
   if (args[0] == "ayudar"){
-m.reply('Gracias por ayudarme')
+m.reply(`Estas Corriendo a ayudarla, *te caes* andas cojo,\n\n.princesa seguir\n.princesa dejarla?`)
 }
 
 if (args[0] == "dejarla"){
-m.reply('Dejastes que se la llevaran')
+m.reply('Dejastes a la princesa')
 }
 
+if (args[0] == "seguir"){
+m.reply('Te levantas del piso, luego de ese madrazo que te distes, estás corriendo x2, los alcanzaste, los golpeas, pero ellos no se dejan, te tiran al suelo te amarran con un cabo.\n\n.princesa desatarme\n.princesa dejarla')
+}
+
+if (args[0] == "desatarme"){
+m.reply('Oh, te has desatado, ahora le estás dando duro, los golpeas, \nenemigo: porfavor ya no, me rindo, estás con la princesa\n\n.princesa llevarla\n.princesa dejarla')
+}
+
+if (args[0] == "llevarla"){
+m.reply('Rescataste a la princesa, el rey te está agradeciendo mucho por ayudar a la princesa, de recompensa te va a dar $250')
+users.dolares += 250
+}
         
+
+
+
+
+
 }
 
 handler.help = ['princesa'];
