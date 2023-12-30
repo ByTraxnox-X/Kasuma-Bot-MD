f2d47ebe6f9567c45f998c06953026c832e20667
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-30 06:05:54 +0100

    API

--- plugins/Owner-autoadmin.js
+++ plugins/Owner-autoadmin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
     if (m.fromMe) throw 'Nggk'
-    if (isAdmin) throw '*YA ES ADMIN DEL GRUPO MI Propietario(a) *'
+    if (isAdmin) throw '*YA ES ADMIN DEL GRUPO MI Propietario(a)*'
     await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
   }
   handler.help = ['autoadmin']
