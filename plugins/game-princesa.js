a1a87dc718e232dcd2d623c2efc2258d14826067
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2023-12-31 03:36:51 +0100

    Update game-princesa.js

--- plugins/game-princesa.js
+++ plugins/game-princesa.js
@@ -1,5 +1,5 @@
 
-let handler = async(m, { conn, text, usedPrefix, command }) => {
+let handler = async(m, { conn, text, usedPrefix, command, args }) => {
   if (command == 'rescatarprincesa') {
       throw `
       Hola, soy la princesa, *Ohhh noo*
