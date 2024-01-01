b4962b6f04bd9d71ba3fbd74f06f2bcaeddd6b29
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2024-01-01 02:47:44 +0100

    Update gamer-princesa2.js

--- plugins/gamer-princesa2.js
+++ plugins/gamer-princesa2.js
@@ -9,14 +9,14 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   m.reply('¡Bienvenido a la aventura para rescatar a la princesa!')
   m.reply('Tu misión es encontrar y rescatar a la princesa del malvado dragón.')
 
-  rl.question('¿Deseas entrar al castillo? (si/no)\n'), (respuesta) => {
+  m.reply('¿Deseas entrar al castillo? (si/no)\n'), (respuesta) => {
     if (respuesta.toLowerCase() === 'si') {
       m.reply('Has entrado al castillo. Encuentras un pasillo oscuro. ¿Deseas avanzar? (si/no)');
       rl.question('', (respuesta) => {
         if (respuesta.toLowerCase() === 'si') {
           m.reply('En el pasillo encuentras una puerta. Abres la puerta y encuentras al dragón.');
           m.reply('El dragón te reta a un juego de adivinanzas. Si adivinas la respuesta correcta, podrás rescatar a la princesa.');
-          rl.question('¿Qué tienes en tus bolsillos? (pista: algo que comienza con la letra "ll")\n', (respuesta) => {
+          m.reply('¿Qué tienes en tus bolsillos? (pista: algo que comienza con la letra "ll")\n', (respuesta) => {
             if (respuesta.toLowerCase() === 'llaves') {
               m.reply('¡Correcto! Has rescatado a la princesa. ¡Felicidades!');
               rl.close();
