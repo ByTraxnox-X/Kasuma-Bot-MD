adcc918b383a4376c7e310a04db7ea966350d6ae
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-01 03:14:46 -0500

    Actualizar game-leon.js

--- plugins/game-leon.js
+++ plugins/game-leon.js
@@ -24,7 +24,7 @@ let handler = async(m, { conn, text, pushname, sleep, usedPrefix, command, args 
 
   }
 
-  if (command == 'princesa') {
+  if (command == 'leon') {
       let users = global.db.data.users[m.sender]
 
 
