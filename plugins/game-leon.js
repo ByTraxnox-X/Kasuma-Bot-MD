060ac39868ca1cd3f1332f8cab3020712e89eaf6
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-01 02:36:24 -0500

    Actualizar game-leon.js

--- plugins/game-leon.js
+++ plugins/game-leon.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text, sleep, usedPrefix, command, args }) => {
   conn.leons = conn.leons ? conn.leons : {}
 
-import faker from 'faker';
+import faker from ‘faker’
 
 
 // Generar un nombre aleatorio
