4b620a7ce5b12626b4f67d8b6c61fff96d36b12b
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2024-01-01 18:39:49 +0100

    KASUMA

--- plugins/game-leon.js
+++ plugins/game-leon.js
@@ -1,67 +1,53 @@
-let handler = async(m, { conn, text, usedPrefix, command, args }) => {
+let handler = async (m, { conn, text, usedPrefix, command, args }) => {
   conn.leons = conn.leons ? conn.leons : {}
 
-let toUser = `${m.sender.split("@")[0]}`
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-
- 
   if (command == 'leonsuelto') {
-
-    m.reply(`*Hola, ${toUser} Soy Sebastian, Ayudame un leon me esta persiguiendo, y ya no puedo mas!!!`)
-
-    m.reply(`*Hola, ${toUser} Soy Parqui, Ayudame un leon me esta persiguiendo, y ya no puedo mas!!!`)
-
-      throw `
-      Para hacer algo por el, usa una de estas opciones
-      
-        *${usedPrefix + 'leon'} ayudar*
-        *${usedPrefix + 'leon'} dejarlo*
-      `
-
+    m.reply(`*Hola, ${who.split`@`[0]} Soy Sebastian, Ayudame un leon me esta persiguiendo, y ya no puedo mas!!!` , { mentions: [who] })
+    m.reply(`*Hola, ${who.split`@`[0]} Soy Parqui, Ayudame un leon me esta persiguiendo, y ya no puedo mas!!!` , { mentions: [who] })
+    throw `
+      Para hacer algo por él, usa una de estas opciones
+      *${usedPrefix + 'leon'} ayudar*
+      *${usedPrefix + 'leon'} dejarlo*
+    `
   }
 
   if (command == 'leon') {
-      let users = global.db.data.users[m.sender]
+    let users = global.db.data.users[m.sender]
 
-
-
-      if (args[0] == "ayudar"){
-    m.reply(`Estas Intentando Detener Al Leon Para Que No Mate A ${toUser}, Nesesitas un arma? Para comprar use\n\n.leon arma`)
+    if (args[0] == "ayudar") {
+      m.reply(`Estas Intentando Detener Al Leon Para Que No Mate A ${who.split`@`[0]}, ¿Necesitas un arma? Para comprar usa\n\n.leon arma`, { mentions: [who] })
     }
 
-    if (args[0] == "dejarlo"){
+    if (args[0] == "dejarlo") {
       let din = Math.floor(Math.random() * 10)
       global.db.data.users[who].dolares -= din
-      
-    m.reply(`Dejastes que el leon lo matara, por lo que la policía eres sospechoso de la muerte, por lo cual perdiste $${din} Dolares.`)
+      m.reply(`Dejaste que el leon lo matara, por lo que la policía eres sospechoso de la muerte, por lo cual perdiste $${din} Dolares.`)
     }
 
-    if (args[0] == "arma"){
+    if (args[0] == "arma") {
       let arm = Math.floor(Math.random() * 20)
       global.db.data.users[who].dolares -= arm
-    m.reply(`Has Comprado Un Arma A ${arm} Dolares. Ahora Usa Estas Opciones De Abajo.\n\n.leon disparar\n.leon no disparar`)
+      m.reply(`Has Comprado Un Arma A ${arm} Dolares. Ahora Usa Estas Opciones De Abajo.\n\n.leon disparar\n.leon no disparar`)
     }
 
-    if (args[0] == "disparar"){
+    if (args[0] == "disparar") {
       let user = global.db.data.users[who]
       let pointAttacker = Math.floor(Math.random() * 200)
       global.db.data.users[who].hp -= pointAttacker
-    m.reply(`Le has disparado al leon 🦁\nOh nooo El leon aun sigue vivo y con ${user.hp}\nDebes Atacar Otra Vez Al Leon Para Matarlo\n.leon disparar`)
 
-    await 5000
-    
-    let  target = args[0]
-    let gan = Math.floor(Math.random() * 500)
-      global.db.data.users[who].dolares += gan
-    if (global.db.data.users[who].hp <= 0) conn.reply(m.chat,`*Felicidades*\nEl leon acaba de morir porque se le acabaron los HP (puntos de salud), por lo a la persona que salvaste decidió darte $${gan}*`,m, {contextInfo : {mentionedJid : [target]}})
-    
+      if (global.db.data.users[who].hp <= 0) {
+        let target = args[0]
+        let gan = Math.floor(Math.random() * 500)
+        global.db.data.users[who].dolares += gan
+        conn.reply(m.chat, `*Felicidades*\nEl leon acaba de morir porque se le acabaron los HP (puntos de salud), por lo que la persona que salvaste decidió darte $${gan}*`, m, { contextInfo: { mentionedJid: [target] } })
+      } else {
+        m.reply(`Le has disparado al leon 🦁\nOh nooo El leon aun sigue vivo y con ${user.hp}\nDebes Atacar Otra Vez Al Leon Para Matarlo\n.leon disparar`)
+        await new Promise(resolve => setTimeout(resolve, 5000))
+      }
     }
-
-
   }
-
-
 }
 
 handler.help = handler.command = ['leonsuelto', 'leon']
