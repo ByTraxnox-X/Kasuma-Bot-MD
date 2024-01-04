a6aedfbc4f8fa26a31ddb3c6d3ac84c09efebfe7
BOT-TX <120403601+BOT-TX@users.noreply.github.com>
2024-01-04 10:16:07 -0500

    KASUMA

--- plugins/game-caracruzpvp2.js
+++ plugins/game-caracruzpvp2.js
@@ -4,13 +4,12 @@ let poin_lose = -100
 let poin_bot = 200
 global.caracruzpvp = global.caracruzpvp ? global.caracruzpvp : {}
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-//let pp = 'https://telegra.ph/file/c7924bf0e0d839290cc51.jpg'
-if (Object.values(conn.caracruzpvp).find(room => room.id.startsWith('caracruzpvp') && [room.p, room.p2].includes(m.sender))) throw `Termina Tu Partida Primero`
-let textquien = `Etiqueta a la persona que quieres desafiar\n\n*Ejemplo:*\n${usedPrefix + command} @`
+if (Object.values(conn.caracruzpvp).find(room => room.id.startsWith('caracruzpvp') && [room.p, room.p2].includes(m.sender))) throw `Tienes un juego en curso, terminalo para empezar otro!`
+let textquien = `Etiqueta a la persona a desafiar!`
 if (!m.mentionedJid[0]) return m.reply(textquien, m.chat, {quoted: m }, { mentions: conn.parseMention(textquien)})
-if (Object.values(conn.caracruzpvp).find(room => room.id.startsWith('caracruzpvp') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `La persona que quieres desafíar esta en otra partida, espera a que termine de jugar`
+if (Object.values(conn.caracruzpvp).find(room => room.id.startsWith('caracruzpvp') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `La persona a desafiar se encuentra en otro Cara o Cruz, etiquete a otro oponente!`
 let id = 'caracruzpvp_' + new Date() * 1
-let caption = `🎮👾 CARA O CRUZ PVP 🎮👾\n\n@${m.sender.split`@`[0]} Desafia a @${m.mentionedJid[0].split`@`[0]} a un (cara o cruz pvp) de cara o cruz\n\n_*Escribe (aceptar) para aceptar*_\n_*Escribe (rechazar) para rechazar*_`
+let caption = `*🎮👾 CARA O CRUZ PVP 🎮👾*\n\n@${m.sender.split`@`[0]} has desafiado a @${m.mentionedJid[0].split`@`[0]} a un enfrentamiento en cara o cruz.\nPara confirmar su participacion escribe "aceptar"`
 conn.caracruzpvp[id] = {
 chat: await conn.sendMessage(m.chat, { text: caption }, {mentions: conn.parseMention(caption)}),
 id: id,
@@ -18,7 +17,7 @@ p: m.sender,
 p2: m.mentionedJid[0],
 status: 'wait',
 waktu: setTimeout(() => {
-if (conn.caracruzpvp[id]) conn.reply(m.chat, `⏳ Tiempo De Espera Finalizado, El Cara o Cruz PvP Se Cancela Por Falta De Respuesta`, m)
+if (conn.caracruzpvp[id]) conn.reply(m.chat, `Tiempo de espera finalizado, vuelvan a crear una sesion si desean jugar.`, m)
   
 delete conn.caracruzpvp[id]
 }, timeout), poin, poin_lose, poin_bot, timeout
