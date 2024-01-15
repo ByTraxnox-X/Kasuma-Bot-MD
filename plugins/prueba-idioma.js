25f4f5d5b4db2a8348ac3ef58539e5513e0f868f
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-15 11:43:10 -0500

    Actualizar plugins

---
+++ plugins/prueba-idioma.js
@@ -0,0 +1,9 @@
+const handler = async (m, {text, command, args, usedPrefix}) => {
+let language = 'es'
+m.reply(`${lenguaje['p1']()}`)
+} 
+let language = 'en'
+m.reply(`${lenguaje['p1']()}`)
+} 
+handler.command = /^(lenguaje)$/i
+export default handler
