17c20fd0eaa7367a2f8fd4168bd96e1eda8ecdf1
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-15 12:07:49 -0500

    Actualizar kasu-ingles.js

--- idiomas/kasu-ingles.js
+++ idiomas/kasu-ingles.js
@@ -2,7 +2,8 @@ const lenguaje = () => { return 'en' } //Ingles
 
 const idioma2 = () => { return `Hello how are you, I'm Kasuma` } 
 
+const AvisoMG = () => { return `MISUSED ACTION\n\n` }
 
 
 
-module.exports = { idioma2 }
+module.exports = { idioma2. AvisoMG }
