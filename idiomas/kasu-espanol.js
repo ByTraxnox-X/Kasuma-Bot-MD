dbca1bf89c6af2e6853a57c4db584bd207088cc3
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-15 12:07:17 -0500

    Actualizar kasu-espanol.js

--- idiomas/kasu-espanol.js
+++ idiomas/kasu-espanol.js
@@ -2,7 +2,9 @@ const lenguaje = () => { return 'es' } //Español
 
 const idioma2 = () => { return `Hola como estas soy Kasuma` } 
 
+const AvisoMG = () => { return `ACCION MAL USADA\n\n` }
 
 
 
-module.exports = { idioma2 }
+
+module.exports = { idioma2, AvisoMG }
