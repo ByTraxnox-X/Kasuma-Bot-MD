79f12d61f91c4761d27c98ac5af1ec3f5b7db71a
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-15 20:51:12 -0500

    Delete idiomas/ingles.js

--- idiomas/ingles.js
+++
@@ -1,9 +0,0 @@
-const lenguaje = () => { return 'en' } //Ingles  
-
-const idioma2 = () => { return `Hello how are you, I'm Kasuma` } 
-
-const AvisoMG = () => { return `MISUSED ACTION\n\n` }
-
-
-
-module.exports = { idioma2, AvisoMG }
