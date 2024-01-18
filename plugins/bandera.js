fead729c1e5cfaf5953307bc837840f6da0bb686
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-18 08:38:55 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -2,51 +2,38 @@ import { sticker } from '../lib/sticker.js'
 import fs from 'fs'
 
 let timeout = 40000
-let poin = 10000
+let poin = 5000
 
 let handler = async (m, { conn, usedPrefix }) => {
-    conn.bandera = conn.bandera ? conn.bandera : {}
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
-    if (id in conn.bandera) {
-        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.bandera[id][0])
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
         throw false
     }
-    let bandera = JSON.parse(fs.readFileSync("./src/game/banderas.json"))
-    let json = bandera[Math.floor(Math.random() * bandera.length)]
-    let _clue = json.emoji
+    let tekateki = JSON.parse(fs.readFileSync("./src/game/banderas.json"))
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+    let _clue = json.response
     let clue = _clue.replace(/[A-Za-z]/g, '_')
-    let caption = `
-ⷮ *Adivina la bandera de este pais:*\n*"${jsonfile.pais}"*
-
+    let caption = `*País:* ${json.pais}
 
 *Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *Bono:* +${poin} Exp
 
-Recuerda responder con emoji la bandera!
+Recuerda responder con el nombre correcto y bien escrito!
 `.trim()
-    conn.bandera[id] = [
-
-
-/*const c = await conn.sendFile(m.chat, await sticker(json.img, "", ""), "", "", m)
-                conn.bandera[id] = [
-                        await conn.reply(m.chat, caption, c, m),*/
-
-
-
-
-     await conn.reply(m.chat, caption, m),
-
-
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(async () => {
-            if (conn.bandera[id]) await conn.reply(m.chat, `Se acabó el tiempo!.`, conn.bandera[id][0])
-            delete conn.bandera[id]
+            if (conn.tekateki[id]) await conn.reply(m.chat, `se acabo el tiempo, inténtalo de nuevo! `, conn.tekateki[id][0])
+            delete conn.tekateki[id]
         }, timeout)
     ]
 }
 
 handler.help = ['adivinabandera']
 handler.tags = ['game']
-handler.command = /^(adivinabandera|adivinalabandera|adivinalasbandera|advbandera)$/i
+handler.command = /^(adivinabandera|bandera|banderade|banderapais)$/i
 
 export default handler
