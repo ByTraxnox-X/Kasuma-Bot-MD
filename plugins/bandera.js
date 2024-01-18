b403eb4015db409cb9315296152cfb5ec9477b3f
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-18 11:49:27 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -2,35 +2,33 @@ import { sticker } from '../lib/sticker.js'
 
 import fs from 'fs'
 
-let timeout = 40000
-let poin = 5000
+let timeout = 10000
+let poin = 10000
 
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
     if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Todavía hay un juego sin terminar!', conn.tekateki[id][0])
+        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
         throw false
     }
-    let tekateki = JSON.parse(fs.readFileSync("./src/game/banderas.json"))
+    let tekateki = JSON.parse(fs.readFileSync("./src/game/casos.json"))
     let json = tekateki[Math.floor(Math.random() * tekateki.length)]
-   /* let stickerFile = fs.readFileSync(json.foto)
-await conn.sendFile(m.chat, stickerFile, 'sticker.png', '')*/
-    
     let _clue = json.response
     let clue = _clue.replace(/[A-Za-z]/g, '_')
-    let caption = `*País*: *"${json.response}"*
+    let caption = `*Mira la imagen y escribe el nombre del pais*
+
 
 *Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *Bono:* +${poin} Exp
 
-Recuerda responder con el nombre correcto y bien escrito!
+Recuerda responder con el nombre completo!
 `.trim()
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.reply(m.chat, `¡Se acabó el tiempo, inténtalo de nuevo! `, conn.tekateki[id][0])
+            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!.`, conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
