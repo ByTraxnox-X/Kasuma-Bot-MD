0f5eb9bf231727b29e486ccac4888d75dae3d1f9
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-18 12:38:30 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -24,7 +24,7 @@ Recuerda responder con el nombre completo!
 `.trim()
     conn.tekateki[id] = [
 //stiker = await sticker(img, false, packname, author)
-await this.sendFile(m.chat, sticker { json.foto: true })
+//await this.sendFile(m.chat, sticker { json.foto: true })
 
 await conn.reply(m.chat, caption, m),
 
