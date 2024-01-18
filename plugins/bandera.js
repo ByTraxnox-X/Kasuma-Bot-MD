7331cb9dae94b9a54fd967917e796f45bdd6a703
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-18 11:27:35 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -1,40 +1,41 @@
 import fs from 'fs'
 
 let timeout = 10000
-let poin = 10000
+let poin = 5000
 
 let handler = async (m, { conn, usedPrefix }) => {
-    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    conn.bandera = conn.bandera ? conn.bandera : {}
     let id = m.chat
-    if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
+    if (id in conn.bandera) {
+        conn.reply(m.chat, 'Todavía hay un juego sin terminar!', conn.bandera[id][0])
         throw false
     }
-    let tekateki = JSON.parse(fs.readFileSync("./src/game/banderas.json"))
-    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+    let bandera = JSON.parse(fs.readFileSync("./src/game/banderas.json"))
+    let json = bandera[Math.floor(Math.random() * bandera.length)]
+    let stickerFile = fs.readFileSync(json.foto)
+await conn.sendFile(m.chat, stickerFile, 'sticker.png', '')
+    
     let _clue = json.response
     let clue = _clue.replace(/[A-Za-z]/g, '_')
-    let caption = `
-ⷮ *Holaaa ${json.response}*
+    let caption = `*País*: *"${json.pais}"*
 
 *Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *Bono:* +${poin} Exp
 
-Recuerda responder con el nombre!
+Recuerda responder con el nombre correcto y bien escrito!
 `.trim()
-    conn.tekateki[id] = [
+    conn.bandera[id] = [
        await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta resolver de nuevo un caso policiaco.`, conn.tekateki[id][0])
-            delete conn.tekateki[id]
+            if (conn.bandera[id]) await conn.reply(m.chat, `¡Se acabó el tiempo, inténtalo de nuevo! `, conn.bandera[id][0])
+            delete conn.bandera[id]
         }, timeout)
     ]
 }
 
-
 handler.help = ['adivinabandera']
 handler.tags = ['game']
-handler.command = /^(adivinabandera|bandera|adivinalabandera)$/i
+handler.command = /^(adivinabandera|bandera|banderade|banderapais)$/i
 
 export default handler
