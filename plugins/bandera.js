d0a260cf5cae9d75fcf84d372685fffbbb884791
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-18 21:48:13 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         throw false
     }
     let banderas = JSON.parse(fs.readFileSync("./src/game/banderas.json"))
-    let json = banderas[Math.floor(Math.random() * tekateki.length)]
+    let json = banderas[Math.floor(Math.random() * banderas.length)]
 let banderaurl = `${json.foto}`
 let pais = `${json.pais}`
 
