8c9c79ff330c04ea3cb516b6731f0d96fe153451
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-19 08:26:40 -0500

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -7,7 +7,7 @@ const handler = async (m, { conn, args }) => {
         const apiUrl = `https://skizo.tech/api/tiktok?url=${args[0]}&apikey=kasumabot`;
         const response = await fetch(apiUrl);
         const data = await response.buffer();
-        let json = await response.json();
+        let json = await apiUrl.json();
         
         m.react(rwait)
         m.reply(`Titulo: ${json.data.title}`)
