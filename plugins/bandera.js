25e00b042c73f4dd1befffc2c17244f545f29eea
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-19 21:24:37 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -1,49 +1,51 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-let timeout = 110000
-let poin = 10000
+let timeout = 110000;
+let poin = 10000;
 
 let handler = async (m, { conn, command, usedPrefix, args }) => {
     const apiUrl = 'https://skizo.tech/api/game/tebakbendera?apikey=kasumabot';
 
-conn.tekateki = conn.tekateki ? conn.tekateki : {}
-    let id = m.chat
+    conn.tekateki = conn.tekateki ? conn.tekateki : {};
+    let id = m.chat;
     if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
-        throw false
+        conn.reply(m.chat, '¡Todavía hay un juego sin terminar!', conn.tekateki[id][0]);
+        throw false;
     }
 
-    let timeout = global.db.data.users[m.sender].wait + 40000;
+    let user = global.db.data.users[m.sender] || {};
+    let userWait = user.wait || 0;
+    let timeout = userWait + 40000;
     let textos = `*Adivina el nombre de la bandera de la foto*
 *Nota: Pusimos 2 minutos para poder visualizar la imagen bien ya que esta borrosa, estamos mejorando eso, en muy poco tiempo estara lista con foto hd*
 
 *Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *Bono:* +${poin} Exp
 
-Recuerda responder con el nombre completo!`.trim()
+Recuerda responder con el nombre completo!`.trim();
 
-    
-    conn.tekateki[id] = [
-    
+    conn.tekateki[id] = true;
+
     const response = await fetch(apiUrl);
     const data = await response.json();
-    
-        conn.sendFile(m.chat, data.img, 'bandera.jpg', textos, m);
-    
-    json, poin,
-        setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta resolver de nuevo.`, conn.tekateki[id][0])
-            delete conn.tekateki[id]
-        }, timeout)
 
-    global.db.data.users[m.sender].wait = new Date() * 1;
-    ]
+    conn.sendFile(m.chat, data.img, 'bandera.jpg', textos, m);
+
+    setTimeout(async () => {
+        if (conn.tekateki[id]) {
+            await conn.reply(m.chat, `¡Se acabó el tiempo!, intenta resolver de nuevo.`, conn.tekateki[id][0]);
+            delete conn.tekateki[id];
+        }
+    }, timeout);
+
+    user.wait = new Date() * 1;
+    user.poin = (user.poin || 0) + poin;
+    global.db.data.users[m.sender] = user;
 };
 
+handler.help = ['adivinabandera'];
+handler.tags = ['game'];
+handler.command = /^(adivinabandera|bandera|banderade|banderapais)$/i;
 
-handler.help = ['adivinabandera']
-handler.tags = ['game']
-handler.command = /^(adivinabandera|bandera|banderade|banderapais)$/i
-
-export default handler
+export default handler;
