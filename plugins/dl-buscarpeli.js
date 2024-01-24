104ee8431df6c0b894b62d8e2b4caa88f1954a9b
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-01-24 04:20:13 +0100

    KASUMA

--- plugins/dl-buscarpeli.js
+++ plugins/dl-buscarpeli.js
@@ -6,7 +6,7 @@ const handler = async (m, { conn, text }) => {
   }
 
   try {
-    const apiUrl = `${kasumaapi}/api/movie?text=${encodeURIComponent(text)}&token=KasumaToken`;
+    const apiUrl = `${api}/api/movie?text=${encodeURIComponent(text)}${token}`;
     const response = await fetch(apiUrl);
 
     if (!response.ok) {
