8a28d5cbfd48796fca6160b8404e4db079c75fc2
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-01-26 05:46:04 +0100

    KASUMA

--- plugins/dl-iTunes.js
+++ plugins/dl-iTunes.js
@@ -5,10 +5,8 @@ const handler = async (m, { conn, text }) => {
         throw 'Ingrese el nombre de la canción';
     }
 
-    const apivisionary = "https://visionaryapi.boxmine.xyz/";
-
     try {
-        const apiUrl = `${apivisionary}/api/itunes?text=${encodeURIComponent(text)}`;
+        const apiUrl = `https://api.popcat.xyz/itunes?q=${encodeURIComponent(text)}`;
         const response = await fetch(apiUrl);
 
         if (!response.ok) {
@@ -20,19 +18,19 @@ const handler = async (m, { conn, text }) => {
         m.react(rwait);
 
         const songInfo =
-            `*${json.message.name}*\n\n` +
-            `*Nombre:* ${json.message.name}\n` +
-            `*Artista:* ${json.message.artist}\n` +
-            `*Álbum:* ${json.message.album}\n` +
-            `*Fecha de lanzamiento:* ${json.message.release_date}\n` +
-            `*Precio:* ${json.message.price}\n` +
-            `*Duración:* ${json.message.length}\n` +
-            `*Género:* ${json.message.genre}\n` +
-            `*Enlace:* ${json.message.url}`;
+            `*${json.name}*\n\n` +
+            `*Nombre:* ${json.name}\n` +
+            `*Artista:* ${json.artist}\n` +
+            `*Álbum:* ${json.album}\n` +
+            `*Fecha de lanzamiento:* ${json.release_date}\n` +
+            `*Precio:* ${json.price}\n` +
+            `*Duración:* ${json.length}\n` +
+            `*Género:* ${json.genre}\n` +
+            `*Enlace:* ${json.url}`;
 
-        if (json.message.thumbnail) {
+        if (json.thumbnail) {
             m.react(done);
-            await conn.sendFile(m.chat, json.message.thumbnail, 'thumbnail.jpg', songInfo, m);
+            await conn.sendFile(m.chat, json.thumbnail, 'thumbnail.jpg', songInfo, m);
         } else {
             m.reply(songInfo);
         }
