310caed44bfa8890be7cad01dfd7fd76c4908569
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-01-26 05:42:09 +0100

    KASUMA

--- handler.js
+++ handler.js
@@ -523,7 +523,7 @@ export async function participantsUpdate({ id, participants, action }) {
                 for (let user of participants) {
                     let pp = 'https://i.imgur.com/nHHUm1a.png'
                     let bg = 'https://cdn.discordapp.com/attachments/850808002545319957/859359637106065408/bg.png'
-                    let members = +groupMetadata.participants.length
+
                     try {
                         pp = await this.profilePictureUrl(user, 'image')
                         } finally {
@@ -541,7 +541,7 @@ export async function participantsUpdate({ id, participants, action }) {
                             let lea = API(`https://api.popcat.xyz`, '/welcomecard', {
                                 background: bg,
                                 text1: "GOOD BYE",
-                                text2: `USUARIOS%20${members}`,
+                                text2: ` `,
                                 text3: " ",
                                 avatar: pp,
                             })
