88971a66c569cae2ee40c0506efaa14b7dbf7440
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-01-30 13:21:55 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -1,78 +1,46 @@
-import fetch from 'node-fetch';
+import fs from 'fs'
 
-const timeoutDuration = 11000;
-const bonusPoints = 10000;
+let timeout = 30000
+let poin = 10000
 
-const getApiData = async () => {
-    const apiUrl = 'https://skizo.tech/api/game/tebakbendera?apikey=kasumabot';
-    const response = await fetch(apiUrl);
-    return response.json();
-};
-
-const sendFlagImage = (conn, m, imgUrl, textos) => {
-    conn.sendFile(m.chat, imgUrl, 'bandera.jpg', textos, m);
-};
-
-const checkAnswer = async (conn, m, userAnswer, correctAnswer) => {
-    const isCorrect = userAnswer.trim().toLowerCase() === correctAnswer.toLowerCase();
-    const replyMessage = isCorrect ? `¡Correcto! Has acertado.` : `¡Se acabó el tiempo!, intenta resolver de nuevo.`;
-    await conn.reply(m.chat, replyMessage, conn.tekateki[m.chat][0]);
-
-    // Puedes realizar acciones adicionales para cuando el usuario acierta aquí si es necesario
-    if (isCorrect) {
-        // Acciones adicionales...
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.apiUrl = conn.apiUrl ? conn.apiUrl : {}
+    let id = m.chat
+    if (id in conn.apiUrl) {
+        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.apiUrl[id][0])
+        throw false
     }
-
-    delete conn.tekateki[m.chat];
-};
-
-const startFlagGame = async (conn, m) => {
-    const id = m.chat;
-
-    if (id in conn.tekateki) {
-        conn.reply(m.chat, '¡Todavía hay un juego sin terminar!', conn.tekateki[id][0]);
-        throw false;
-    }
-
-    const user = global.db.data.users[m.sender] || {};
-    const userWait = user.wait || 0;
-    const timeout = userWait + 40000;
-    const textos = `*Adivina el nombre de la bandera de la foto.*
-*Nota: Pusimos 2 minutos para poder visualizar la imagen bien ya que esta borrosa, estamos mejorando eso, en muy poco tiempo estara lista con foto hd*
+    let apiUrl = JSON.parse(fs.readFileSync("./src/game/casos.json"))
+    const apiUrl = 'https://api-kasu.onrender.com/api/game/bandera?apikey=79242cc3';
+  /*  let json = apiUrl[Math.floor(Math.random() * apiUrl.length)]
+    let _clue = json.name
+    let clue = _clue.replace(/[A-Za-z]/g, '_')*/
+    let caption = `
+*Cual es esta bandera?*
 
 *Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
-*Bono:* +${bonusPoints} Exp
+*Bono:* +${poin} Exp
 
-Recuerda responder con el nombre completo!`.trim();
+Recuerda responder con el nombre completo
+`.trim()
+    conn.apiUrl[id] = [
+      // await conn.reply(m.chat, caption, m),
+       
+    const name = await fetch(apiUrl);
+    const data = await name.json();
 
-    conn.tekateki[id] = true;
+    conn.sendFile(m.chat, data.img, 'bandera.jpg', caption, m);
+       
+        json, poin,
+        setTimeout(async () => {
+            if (conn.apiUrl[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta resolver de nuevo.`, conn.apiUrl[id][0])
+            delete conn.apiUrl[id]
+        }, timeout)
+    ]
+}
 
-    const data = await getApiData();
-    sendFlagImage(conn, m, data.img, textos);
+handler.help = ['bandera']
+handler.tags = ['game']
+handler.command = /^(bandera)$/i
 
-    setTimeout(async () => {
-        if (conn.tekateki[id]) {
-            const userAnswer = conn.tekateki[id][0].text;
-            await checkAnswer(conn, m, userAnswer, data.name);
-
-            user.wait = new Date() * 1;
-            user.poin = (user.poin || 0) + bonusPoints;
-            global.db.data.users[m.sender] = user;
-
-        }
-    }, timeout);
-};
-
-const handler = async (m, { conn, command, usedPrefix, args }) => {
-    try {
-        await startFlagGame(conn, m);
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-handler.help = ['adivinabandera'];
-handler.tags = ['game'];
-handler.command = /^(adivinabandera|bandera|banderade|banderapais)$/i;
-
-export default handler;
+export default handler
