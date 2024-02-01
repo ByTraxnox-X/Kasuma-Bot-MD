7543668fff2b2cd6ad2e04db6c6ee926e8d8d4f6
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-01 12:12:55 -0500

    Actualizar game-crimen.js

--- plugins/game-crimen.js
+++ plugins/game-crimen.js
@@ -1,6 +1,4 @@
-
-
-import axios from 'axios';
+import fs from 'fs'
 
 let timeout = 180000
 let poin = 10000
@@ -9,37 +7,36 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
     if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Todavía hay un juego sin terminar!', conn.tekateki[id][0])
+        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
         throw false
     }
-    try {
-        let res = await axios.get('https://apikasu.onrender.com/api/game/bandera?apikey=SebastianDevelop');
-        let json = res.data;
-        let imageUrl = json.img;
-        let caption = `
-ⷮ *Bandera Mystery*
+    let tekateki = JSON.parse(fs.readFileSync("./src/game/casos.json"))
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
+    let _clue = json.response
+    let clue = _clue.replace(/[A-Za-z]/g, '_')
+    let caption = `
+ⷮ *${json.caso}*
 
-Adivina la bandera de qué país es esta?
+*Sospechosos:*
+ⷮ${json.Sospechosos}
 
 *Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *Bono:* +${poin} Exp
-`.trim();
-        conn.tekateki[id] = [
-            await conn.sendFile(m.chat, imageUrl, 'bandera.jpg', caption, m),
-            json, poin,
-            setTimeout(async () => {
-                if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta adivinar la bandera de nuevo.`, conn.tekateki[id][0])
-                delete conn.tekateki[id]
-            }, timeout)
-        ]
-    } catch (e) {
-        console.error(e)
-        conn.reply(m.chat, 'Ocurrió un error al cargar la bandera. Inténtalo de nuevo más tarde.', m)
-    }
+
+Recuerda responder con el nombre completo del presunto culpable!
+`.trim()
+    conn.tekateki[id] = [
+       await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta resolver de nuevo un caso policiaco.`, conn.tekateki[id][0])
+            delete conn.tekateki[id]
+        }, timeout)
+    ]
 }
 
-handler.help = ['bandera']
+handler.help = ['caso']
 handler.tags = ['game']
-handler.command = /^(bandera|adivinarbandera|flag)$/i
+handler.command = /^(caso|casoinvestigar|casopoliciaco|investigarcaso)$/i
 
 export default handler
