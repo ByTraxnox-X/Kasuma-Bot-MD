9fdd2515ed842b6f6cafd3b3fdda60a7571992bd
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-01 12:13:57 -0500

    Actualizar bandera.js

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -1,44 +1,44 @@
-import fs from 'fs'
 
-let timeout = 110000
+import axios from 'axios';
+
+let timeout = 180000
 let poin = 10000
 
 let handler = async (m, { conn, usedPrefix }) => {
-
-    const apiUrl = 'https://apikasu.onrender.com/api/game/bandera?apikey=SebastianDevelop';
-    const name = await fetch(apiUrl);
-    const data = await name.json();
-
-   // conn.tekateki = conn.tekateki ? conn.tekateki : {}
-   /* let id = m.chat
+    conn.tekateki = conn.tekateki ? conn.tekateki : {}
+    let id = m.chat
     if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
+        conn.reply(m.chat, 'Todavía hay un juego sin terminar!', conn.tekateki[id][0])
         throw false
-    }*/
-    let textos = `
-ⷮ *Adivina el nombre de la bandera de la foto*
-*Nota: Pusimos 2 minutos para poder visualizar la imagen bien ya que esta borrosa, estamos mejorando eso, en muy poco tiempo estara lista con foto hd*
+    }
+    try {
+        let res = await axios.get('https://apikasu.onrender.com/api/game/bandera?apikey=SebastianDevelop');
+        let json = res.data;
+        let imageUrl = json.img;
+        let caption = `
+ⷮ *Bandera Mystery*
 
+Adivina la bandera de qué país es esta?
 
 *Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
 *Bono:* +${poin} Exp
-
-Recuerda responder con el nombre completo!
-`.trim()
-    conn.tekateki[id] = [
-       
-       conn.sendFile(m.chat, data.img, 'bandera.jpg', textos, m);
-       
-        json, poin,
-        setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta resolver de nuevo.`, conn.tekateki[id][0])
-            //delete conn.tekateki[id]
-        }, timeout)
-    ]
+`.trim();
+        conn.tekateki[id] = [
+            await conn.sendFile(m.chat, imageUrl, 'bandera.jpg', caption, m),
+            json, poin,
+            setTimeout(async () => {
+                if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!, intenta adivinar la bandera de nuevo.`, conn.tekateki[id][0])
+                delete conn.tekateki[id]
+            }, timeout)
+        ]
+    } catch (e) {
+        console.error(e)
+        conn.reply(m.chat, 'Ocurrió un error al cargar la bandera. Inténtalo de nuevo más tarde.', m)
+    }
 }
 
-handler.help = ['adivinabandera']
+handler.help = ['bandera']
 handler.tags = ['game']
-handler.command = /^(adivinabandera|bandera|banderade|banderapais)$/i
+handler.command = /^(bandera|adivinarbandera|flag)$/i
 
 export default handler
