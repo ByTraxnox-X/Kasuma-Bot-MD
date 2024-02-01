834c65888a2778ba1274774de2f3e847930e1629
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-01 18:59:12 +0100

    API

--- plugins/bandera.js
+++ plugins/bandera.js
@@ -4,16 +4,16 @@ let handler = async (m, { conn }) => {
     try {
         let response = await axios.get('https://apikasu.onrender.com/api/game/bandera?apikey=SebastianDevelop');
 
-        if (response.status === 200 && response.data && response.data.img) {
-            let imgURL = response.data.img;
+        if (response.status === 200 && response.result && response.result.img) {
+            let imgURL = response.result.img;
             conn.sendFile(m.chat, imgURL, 'bandera.jpg', '¿Cuál es el nombre de esta bandera?');
 
             // Espera la respuesta del usuario
             conn.waitForReply(m.chat, async (reply) => {
-                if (reply.text.trim().toLowerCase() === response.data.respuesta.toLowerCase()) {
-                    conn.reply(m.chat, `¡Correcto! La bandera es ${response.data.respuesta}.`, m);
+                if (reply.text.trim().toLowerCase() === response.result.respuesta.toLowerCase()) {
+                    conn.reply(m.chat, `¡Correcto! La bandera es ${response.result.respuesta}.`, m);
                 } else {
-                    conn.reply(m.chat, `Incorrecto. La bandera es ${response.data.respuesta}.`, m);
+                    conn.reply(m.chat, `Incorrecto. La bandera es ${response.result.respuesta}.`, m);
                 }
             });
         } else {
@@ -25,7 +25,8 @@ let handler = async (m, { conn }) => {
     }
 }
 
-handler.command = /^\bandera$/i
-handler.tags = ['game']
 handler.help = ['bandera']
+handler.tags = ['game']
+handler.command = ["bandera"]
+
 export default handler;
