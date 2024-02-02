789427526dba04ad62d1acd6a9705cbaf1e6e942
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-02 23:19:38 +0100

    KASUMA

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -6,25 +6,23 @@ const handler = async (m, { conn, args }) => {
     }
 
     try {
-        const apiUrl = `https://vihangayt.me/download/instagram2?url=${args[0]}`;
+        const apiUrl = `${apikasu}/api/dowloader/instagram?url=${args[0]}&apikey=${apikeykasu}`
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        
+        const responseData = await response.json();
+
         m.react(rwait);
 
-        if (data.status && data.data.length > 0) {
-            for (const media of data.data) {
-                const fileType = media.type === 'video' ? 'video.mp4' : 'imagen.jpg';
+        if (responseData.status && responseData.result.length > 0) {
+            for (const media of responseData.result) {
                 m.react(done);
-
-                await conn.sendFile(m.chat, media.download_link, fileType, '', m);
+                await conn.sendFile(m.chat, media.link, media.ext === 'mp4' ? 'video.mp4' : 'imagen.jpg', '', m);
             }
         } else {
             throw 'No se pudo obtener el contenido de Instagram.';
         }
     } catch (error) {
         console.error(error);
-        throw `Ocurrió un error al procesar la solicitud: ${error.message}`;
+        throw `Ocurrió un error al procesar la solicitud: ${error.message};`
     }
 };
 
@@ -32,4 +30,4 @@ handler.help = ['instagram'];
 handler.tags = ['dl'];
 handler.command = /^(instagramdl|instagram|igdl|ig)$/i;
 
-export default handler;
+export default handler;
