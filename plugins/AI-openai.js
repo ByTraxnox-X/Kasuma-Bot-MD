310a18e9e80939058db95d6c3c64a9dbe62b745f
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-02 04:57:07 +0100

    KASUMA

--- plugins/AI-openai.js
+++ plugins/AI-openai.js
@@ -8,7 +8,7 @@ const handler = async (m, { conn, text }) => {
   try {
     conn.sendPresenceUpdate('composing', m.chat);
 
-    const apiUrl = `https://vanitas-api.boxmine.xyz/api/chatgpt?text=${text}&apikey=Vanitas`;
+    const apiUrl = `https://vanitas.website/api/chatgpt?text=${text}&apikey=Vanitas`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
