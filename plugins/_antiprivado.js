3823d9a7288801b13ee04ebe0af2ecd98e1796fc
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-02 05:28:16 +0100

    KASUMA

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,8 +6,7 @@ if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TI
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`*hola @${m.sender.split`@`[0]}*, hablar con el bot en el privado es ilegal por tanto seras bloqueado, si quieres usar el bot unete a nuestra comunidad: ${wagp}
-${canal}`, false, { mentions: [m.sender] })
+await m.reply(`*hola @${m.sender.split`@`[0]}*, hablar con el bot en el privado es ilegal por tanto seras bloqueado, si quieres usar el bot unete a nuestra comunidad: ${wagp}`, false, { mentions: [m.sender] })
 await this.updateBlockStatus(m.chat, 'block')}
 return !1
 }
