a0db641d3f6a20ff92a88be606928d2e97bd5ca1
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-02 13:42:34 -0500

    Actualizar owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -1,25 +1,44 @@
-const handler = async (m, {text, conn, usedPrefix, command}) => {
-  const why = `uso correcto\n${usedPrefix + command} @${m.sender.split('@')[0]}`;
+const handler = async (m, { text, conn, usedPrefix, command }) => {
+  const why = `Uso correcto\n${usedPrefix + command} @${m.sender.split('@')[0]}`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
-  if (!who) conn.reply(m.chat, why, m, {mentions: [m.sender]});
+  if (!who) conn.reply(m.chat, why, m, { mentions: [m.sender] });
+
   const res = [];
+
   switch (command) {
-    case 'blok': case 'block':
+    case 'blok':
+    case 'block':
       if (who) {
         await conn.updateBlockStatus(who, 'block').then(() => {
           res.push(who);
+          // Guardar información en la base de datos
+          global.db.data.blockedUsers = global.db.data.blockedUsers || {};
+          global.db.data.blockedUsers[who] = global.db.data.blockedUsers[who] || [];
+          global.db.data.blockedUsers[who].push(m.chat); // Guarda el ID del grupo en el que se bloqueó al usuario
+          global.db.save();
         });
-      } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
+      } else conn.reply(m.chat, why, m, { mentions: [m.sender] });
       break;
-    case 'unblok': case 'unblock':
+    case 'unblok':
+    case 'unblock':
       if (who) {
         await conn.updateBlockStatus(who, 'unblock').then(() => {
           res.push(who);
+          // Elimina la información del grupo de la base de datos al desbloquear
+          if (global.db.data.blockedUsers && global.db.data.blockedUsers[who]) {
+            const index = global.db.data.blockedUsers[who].indexOf(m.chat);
+            if (index !== -1) {
+              global.db.data.blockedUsers[who].splice(index, 1);
+              if (global.db.data.blockedUsers[who].length === 0) delete global.db.data.blockedUsers[who];
+              global.db.save();
+            }
+          }
         });
-      } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
+      } else conn.reply(m.chat, why, m, { mentions: [m.sender] });
       break;
   }
-  if (res[0]) conn.reply(m.chat, `*se completo la operacion (${command}) con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+
+  if (res[0]) conn.reply(m.chat, `*Se completó la operación (${command}) con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, { mentions: res });
 };
 
 handler.help = ['block/unblock (@user)']
@@ -27,4 +46,4 @@ handler.tags = ['owner']
 handler.command = /^(block|unblock)$/i;
 handler.rowner = true
 
-export default handler
+export default handler;
