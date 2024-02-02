9298fd74a1999ca3e0033bd2fc7a6c19ead5af38
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-02 14:05:09 -0500

    Actualizar owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -1,49 +1,44 @@
-const handler = async (m, { text, conn, usedPrefix, command }) => {
-  const why = `Uso correcto\n${usedPrefix + command} @${m.sender.split('@')[0]}`;
-  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
-  if (!who) conn.reply(m.chat, why, m, { mentions: [m.sender] });
+const handler = async (m, { conn, command }) => {
+  const why = `Uso correcto\n${command} @usuario`;
+  const who = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : null);
+  if (!who) return conn.reply(m.chat, why, m, { mentions: [m.sender] });
 
-  const res = [];
-  let responseSent = false; // Variable para controlar si ya se envió la respuesta
+  let res = [];
+  let groupName;
 
   switch (command) {
     case 'blok':
     case 'block':
-      if (who) {
-        await conn.updateBlockStatus(who, 'block').then(() => {
-          res.push(who);
-          // Guardar información en la base de datos
-          global.db.data.blockedUsers = global.db.data.blockedUsers || {};
-          global.db.data.blockedUsers[who] = global.db.data.blockedUsers[who] || [];
-          global.db.data.blockedUsers[who].push(m.chat); // Guarda el ID del grupo en el que se bloqueó al usuario
-          fs.writeFileSync('./lib/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8'); // Guarda la base de datos de forma síncrona
-          if (!responseSent) {
-            conn.reply(m.chat, `*Se completó la operación (${command}) con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, { mentions: res });
-            responseSent = true; // Marcar que ya se envió la respuesta
-          }
-        });
-      } else conn.reply(m.chat, why, m, { mentions: [m.sender] });
+      await conn.updateBlockStatus(who, 'block').then(() => {
+        res.push(who);
+        groupName = m.isGroup ? m.chat : null;
+      });
       break;
     case 'unblok':
     case 'unblock':
-      if (who) {
-        await conn.updateBlockStatus(who, 'unblock').then(() => {
-          res.push(who);
-          // Elimina la información del grupo de la base de datos al desbloquear
-          if (global.db.data.blockedUsers && global.db.data.blockedUsers[who]) {
-            const index = global.db.data.blockedUsers[who].indexOf(m.chat);
-            if (index !== -1) {
-              global.db.data.blockedUsers[who].splice(index, 1);
-              if (global.db.data.blockedUsers[who].length === 0) delete global.db.data.blockedUsers[who];
-              fs.writeFileSync('./path/to/your/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8'); // Guarda la base de datos de forma síncrona
-              if (!responseSent) {
-                conn.reply(m.chat, `*Se completó la operación (${command}) con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, { mentions: res });
-                responseSent = true; // Marcar que ya se envió la respuesta
-              }
-            }
-          }
-        });
-      } else conn.reply(m.chat, why, m, { mentions: [m.sender] });
+      await conn.updateBlockStatus(who, 'unblock').then(() => {
+        res.push(who);
+        groupName = m.isGroup ? m.chat : null;
+      });
       break;
   }
+
+  if (res.length > 0) {
+    let replyText = `*Operación (${command}) completada con ${res.map(v => '@' + v.split('@')[0]).join(', ')}*`;
+    if (groupName) {
+      const group = await conn.getName(groupName);
+      replyText += `\nEn el grupo: ${group}`;
+    }
+
+    conn.reply(m.chat, replyText, m, { mentions: res });
+  } else {
+    conn.reply(m.chat, why, m, { mentions: [m.sender] });
+  }
 };
+
+handler.help = ['block/unblock (@usuario)'];
+handler.tags = ['owner'];
+handler.command = /^(block|unblock)$/i;
+handler.rowner = true;
+
+export default handler;
