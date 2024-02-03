3be464ecf76db4b691572de22b1ac199fb277e8f
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-02 22:44:42 -0500

    Actualizar owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -4,21 +4,39 @@ const handler = async (m, { conn, command }) => {
   if (!who) return conn.reply(m.chat, why, m, { mentions: [m.sender] });
 
   let res = [];
-  let groupName;
+  let groupName = m.isGroup ? m.chat : null;
 
   switch (command) {
     case 'blok':
     case 'block':
       await conn.updateBlockStatus(who, 'block').then(() => {
         res.push(who);
-        groupName = m.isGroup ? m.chat : null;
+        if (groupName) {
+          // Guardar información en la base de datos solo si es un grupo
+          global.db.data.blockedUsers = global.db.data.blockedUsers || {};
+          global.db.data.blockedUsers[who] = global.db.data.blockedUsers[who] || [];
+          if (!global.db.data.blockedUsers[who].includes(groupName)) {
+            global.db.data.blockedUsers[who].push(groupName); // Guarda el nombre del grupo
+          }
+          fs.writeFileSync('./lib/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8'); // Guarda la base de datos de forma síncrona
+        }
       });
       break;
     case 'unblok':
     case 'unblock':
       await conn.updateBlockStatus(who, 'unblock').then(() => {
         res.push(who);
-        groupName = m.isGroup ? m.chat : null;
+        if (groupName) {
+          // Eliminar la información del grupo de la base de datos solo si es un grupo
+          if (global.db.data.blockedUsers && global.db.data.blockedUsers[who]) {
+            const index = global.db.data.blockedUsers[who].indexOf(groupName);
+            if (index !== -1) {
+              global.db.data.blockedUsers[who].splice(index, 1);
+              if (global.db.data.blockedUsers[who].length === 0) delete global.db.data.blockedUsers[who];
+              fs.writeFileSync('./path/to/your/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8'); // Guarda la base de datos de forma síncrona
+            }
+          }
+        }
       });
       break;
   }
@@ -26,8 +44,7 @@ const handler = async (m, { conn, command }) => {
   if (res.length > 0) {
     let replyText = `*Operación (${command}) completada con ${res.map(v => '@' + v.split('@')[0]).join(', ')}*`;
     if (groupName) {
-      const group = await conn.getName(groupName);
-      replyText += `\nEn el grupo: ${group}`;
+      replyText += `\nEn el grupo: ${groupName}`;
     }
 
     conn.reply(m.chat, replyText, m, { mentions: res });
