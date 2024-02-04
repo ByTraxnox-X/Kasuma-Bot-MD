705c2f6c216650d4e4f7a91423202fd7f5d51c73
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-04 14:40:56 -0500

    Actualizar owner-grupos.js

--- plugins/owner-grupos.js
+++ plugins/owner-grupos.js
@@ -9,19 +9,18 @@ const handler = async (m, { conn }) => {
   for (const group of groups) {
     const groupName = group.subject || group.id;
 
-    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${group.id} [${group.metadata?.read_only ? 'No participa' : 'Participa'}`;
-
     // Verificar y mostrar usuarios bloqueados en este grupo
+    let blockedTxt = '';
     if (global.db.data.blockedUsers) {
       const blockedUsers = global.db.data.blockedUsers[conn.user.jid];
       const blockedUsersInGroup = blockedUsers?.filter(user => user.endsWith('@g.us') && group.participants.find(p => p.jid === user));
 
       if (blockedUsersInGroup && blockedUsersInGroup.length > 0) {
-        txt += `, Bloqueados: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
+        blockedTxt = `, Bloqueados: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
       }
     }
 
-    txt += ']\n\n';
+    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${group.id} [${group.metadata?.read_only ? 'No participa' : 'Participa'}${blockedTxt}]\n\n`;
   }
 
   conn.reply(m.chat, txt.trim(), m);
