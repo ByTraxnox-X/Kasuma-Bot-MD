44e4feeeb52a9ae53f3b5b523cb2caeb010ddaf8
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-04 12:57:08 -0500

    Actualizar owner-grupos.js

--- plugins/owner-grupos.js
+++ plugins/owner-grupos.js
@@ -10,6 +10,7 @@ const isUserBlockedInGroup = (userId, groupName) => {
 
 const handler = async (m, { conn }) => {
   let txt = 'Lista de grupos\n';
+  let blockedTxt = '';
 
   // Obtener la lista de grupos
   const groups = Object.values(await conn.groupFetchAllParticipating());
@@ -18,7 +19,7 @@ const handler = async (m, { conn }) => {
     const groupName = await conn.getName(jid);
     const groupId = jid;
 
-    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${groupId} [${chat?.metadata?.read_only ? 'No participa' : 'Participa'}]\n`;
+    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${groupId} [${chat?.metadata?.read_only ? 'No participa' : 'Participa'}]`;
 
     // Verificar si el usuario y el bot están en el mismo grupo
     const isUserInGroup = groups.find(group => group.id === jid);
@@ -27,15 +28,18 @@ const handler = async (m, { conn }) => {
       if (global.db.data.blockedUsers) {
         const blockedUsers = global.db.data.blockedUsers[conn.user.jid];
         const blockedUsersInGroup = blockedUsers?.filter(user => groups.find(group => group.participants.find(p => p.jid === user) && group.id === jid));
-        
+
         if (blockedUsersInGroup && blockedUsersInGroup.length > 0) {
-          txt += `Bloqueados en el grupo: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}\n`;
+          blockedTxt += `\nBloqueados en ${groupName}: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
         }
       }
     }
 
-    txt += '\n';
+    txt += '\n\n';
   }
+
+  // Agregar la sección de usuarios bloqueados al final del mensaje
+  txt += blockedTxt;
 
   conn.reply(m.chat, txt.trim(), m);
 };
