be85267954a9fb452f2437c6f95b09f4efea3bd1
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-03 19:08:14 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,12 +1,37 @@
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-if (m.isBaileys && m.fromMe) return !0
-if (m.isGroup) return !1
-if (!m.message) return !0
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA')) return !0
-let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[this.user.jid] || {}
-if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`*hola @${m.sender.split`@`[0]}*, hablar con el bot en el privado es ilegal por tanto seras bloqueado, si quieres usar el bot unete a nuestra comunidad: ${wagp}`, false, { mentions: [m.sender] })
-await this.updateBlockStatus(m.chat, 'block')}
-return !1
+export async function before(m, { isOwner, isROwner }) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
+  let bot = global.db.data.settings[this.user.jid] || {};
+
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    const userGroups = [...this.chats.values()].filter(chat => chat.jid.endsWith('g.us') && chat.isGroup && chat.has(m.sender));
+    if (userGroups.length > 0) {
+      const groupNames = await Promise.all(userGroups.map(group => this.getName(group.jid)));
+      const groupNameString = groupNames.join(', ');
+      await m.reply(
+        `» No está permitido enviar mensajes privados al bot. Serás bloqueado.\n\nEstás en los siguientes grupos: ${groupNameString}`,
+        false,
+        { mentions: [m.sender] }
+      );
+
+      // Guardar información en la base de datos solo si es un grupo
+      global.db.data.blockedUsers = global.db.data.blockedUsers || {};
+      global.db.data.blockedUsers[m.sender] = global.db.data.blockedUsers[m.sender] || [];
+      global.db.data.blockedUsers[m.sender].push(...userGroups.map(group => group.jid));
+      fs.writeFileSync('./lib/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8'); // Guarda la base de datos de forma síncrona
+
+      await this.updateBlockStatus(m.sender, "block");
+    } else {
+      await m.reply(
+        `» No está permitido enviar mensajes privados al bot. Serás bloqueado.`,
+        false,
+        { mentions: [m.sender] }
+      );
+      await this.updateBlockStatus(m.sender, "block");
+    }
+  }
+  return !1;
 }
+
