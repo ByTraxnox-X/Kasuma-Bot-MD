347f42df4176707274987665d061d46746c9b30b
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-03 19:35:30 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,18 +1,34 @@
+const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, usedPrefix, command }) {
+if (m.isBaileys && m.fromMe) return !0
+if (m.isGroup) return !1
+if (!m.message) return !0
+const regex = new RegExp(`^${comandos.source}$`, 'i')
+if (regex.test(m.text.toLowerCase().trim())) return !0
 
-export async function before(m, {isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
-  let bot = global.db.data.settings[this.user.jid] || {};
+let chat, user, bot, mensaje
+chat = global.db.data.chats[m.chat]
+user = global.db.data.users[m.sender]
+bot = global.db.data.settings[this.user.jid] || {}
 
-  if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(
-      `» No está permitido el privado al bot serás bloqueado. ${md}`,
-      false,
-      {mentions: [m.sender]}
-    );
-    await this.updateBlockStatus(m.chat, "block");
-  }
-  return !1;
+if (bot.antiPrivate && !isOwner && !isROwner) {
+if (user.counterPrivate === 0) {
+mensaje = `*@${m.sender.split`@`[0]} ESTÁ PROHIBIDO ESCRIBIR AL PRIVADO, PORQUE ASÍ LO QUISO MI PROPIETARIO(A).*\n\n⚠️ \`\`\`PRIMERA ADVERTENCIA\`\`\` ⚠️`
+await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] })  
+  
+} else if (user.counterPrivate === 1) {
+let grupos = [ nn, nnn, nnnt, nnntt, nnnttt ].getRandom()
+mensaje = `*@${m.sender.split`@`[0]} YA SE MENCIONÓ QUE NO PUEDE ESCRIBIR AL PRIVADO. *SI VUELVE A ESCRIBIR SERÁ BLOQUEADO(A)* ‼️\n⚠️ \`\`\`SEGUNDA ADVERTENCIA\`\`\` ⚠️`
+await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] }) 
+  
+} else if (user.counterPrivate === 2) {
+mensaje = `*@${m.sender.split`@`[0]} SERÁ BLOQUEADO(A). SE MENCIONÓ ANTES QUE NO PODÍA ESCRIBIR AL PRIVADO.*\n\n⚠️ \`\`\`TERCERA ADVERTENCIA\`\`\` ⚠️`
+await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] }) 
+  
+user.counterPrivate = -1
+await this.updateBlockStatus(m.sender, 'block')
+}
+user.counterPrivate++
+}
+return !1
 }
