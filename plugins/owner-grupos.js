c57f374b1043f5e48cb168349909ab510a4643c0
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-04 14:49:24 -0500

    Actualizar owner-grupos.js

--- plugins/owner-grupos.js
+++ plugins/owner-grupos.js
@@ -8,23 +8,31 @@ const handler = async (m, { conn }) => {
 
   for (const group of groups) {
     const groupName = group.subject || group.id;
-
-    let blockedTxt = '';
+    const groupId = group.id;
+    const participationStatus = group.metadata?.read_only ? 'No participa' : 'Participa';
 
     // Verificar y mostrar usuarios bloqueados en este grupo
-    if (global.db.data.blockedUsers) {
-      const blockedUsers = global.db.data.blockedUsers[conn.user.jid];
-      const blockedUsersInGroup = blockedUsers?.filter(user => user.endsWith('@g.us') && group.participants.find(p => p.jid === user));
+    const blockedTxt = getBlockedUsersText(conn.user.jid, groupId);
 
-      if (blockedUsersInGroup && blockedUsersInGroup.length > 0) {
-        blockedTxt = `, Bloqueados: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
-      }
-    }
-
-    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${group.id} [${group.metadata?.read_only ? 'No participa' : 'Participa'}${blockedTxt}]\n\n`;
+    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${groupId} [${participationStatus}${blockedTxt}]\n\n`;
   }
 
   conn.reply(m.chat, txt.trim(), m);
+};
+
+const getBlockedUsersText = (botId, groupId) => {
+  let blockedTxt = '';
+
+  if (global.db.data.blockedUsers) {
+    const blockedUsers = global.db.data.blockedUsers[botId];
+    const blockedUsersInGroup = blockedUsers?.filter(user => user.endsWith('@g.us') && groupId && groupId.includes(user));
+
+    if (blockedUsersInGroup && blockedUsersInGroup.length > 0) {
+      blockedTxt = `, Bloqueados: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
+    }
+  }
+
+  return blockedTxt;
 };
 
 handler.help = ['listagrupos'];
