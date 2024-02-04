eaaa6dcf2f0c2114f51c7c35ce89cd2dcf4c8b59
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-04 14:53:43 -0500

    Actualizar owner-grupos.js

--- plugins/owner-grupos.js
+++ plugins/owner-grupos.js
@@ -1,38 +1,40 @@
 import fs from 'fs';
 
 const handler = async (m, { conn }) => {
-  let txt = 'Lista de grupos\n';
-
-  // Obtener la lista de grupos
   const groups = Object.values(await conn.groupFetchAllParticipating());
+  let txt = 'Lista de grupos\n';
 
   for (const group of groups) {
     const groupName = group.subject || group.id;
     const groupId = group.id;
     const participationStatus = group.metadata?.read_only ? 'No participa' : 'Participa';
 
-    // Verificar y mostrar usuarios bloqueados en este grupo
     const blockedTxt = getBlockedUsersText(conn.user.jid, groupId);
 
     txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${groupId} [${participationStatus}${blockedTxt}]\n\n`;
+  }
+
+  const allBlockedUsersTxt = getAllBlockedUsersText(conn.user.jid);
+  if (allBlockedUsersTxt) {
+    txt += `\nUsuarios bloqueados en todos los grupos: ${allBlockedUsersTxt}\n\n`;
   }
 
   conn.reply(m.chat, txt.trim(), m);
 };
 
 const getBlockedUsersText = (botId, groupId) => {
-  let blockedTxt = '';
+  const blockedUsers = global.db.data.blockedUsers?.[botId];
+  const blockedUsersInGroup = blockedUsers?.filter(user => user.endsWith('@g.us') && groupId && groupId.includes(user));
+  return blockedUsersInGroup && blockedUsersInGroup.length > 0
+    ? `, Bloqueados: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`
+    : '';
+};
 
-  if (global.db.data.blockedUsers) {
-    const blockedUsers = global.db.data.blockedUsers[botId];
-    const blockedUsersInGroup = blockedUsers?.filter(user => user.endsWith('@g.us') && groupId && groupId.includes(user));
-
-    if (blockedUsersInGroup && blockedUsersInGroup.length > 0) {
-      blockedTxt = `, Bloqueados: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
-    }
-  }
-
-  return blockedTxt;
+const getAllBlockedUsersText = (botId) => {
+  const blockedUsers = global.db.data.blockedUsers?.[botId];
+  return blockedUsers && blockedUsers.length > 0
+    ? blockedUsers.map(user => `@${user.split('@')[0]}`).join(', ')
+    : '';
 };
 
 handler.help = ['listagrupos'];
