3472e5b1389736cb4196b0d866337f4f6f021bbe
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-03 19:30:22 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,36 +1,18 @@
-// Código corregido para la función "before"
-export async function before(m, { isOwner, isROwner }) {
+
+export async function before(m, {isOwner, isROwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
   if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
-
   let bot = global.db.data.settings[this.user.jid] || {};
 
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    const userGroups = [...this.chats.values()].filter(chat => chat.jid.endsWith('g.us') && chat.isGroup && chat.has(m.sender));
-    
-    if (userGroups.length > 0) {
-      const groupNames = await Promise.all(userGroups.map(group => this.getName(group.jid)));
-      const groupNameString = groupNames.join(', ');
-
-      if (m.isGroup) {
-        await m.reply(
-          `» No está permitido enviar mensajes privados al bot. Serás bloqueado.\n\nEstás en los siguientes grupos: ${groupNameString}`,
-          false,
-          { mentions: [m.sender] }
-        );
-
-        global.db.data.blockedUsers = global.db.data.blockedUsers || {};
-        global.db.data.blockedUsers[m.sender] = global.db.data.blockedUsers[m.sender] || [];
-        global.db.data.blockedUsers[m.sender].push(...userGroups.map(group => group.jid));
-        fs.writeFileSync('./lib/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8');
-
-        await this.updateBlockStatus(m.sender, "block");
-      } else {
-        await this.updateBlockStatus(m.sender, "block");
-      }
-    }
+    await m.reply(
+      `» No está permitido el privado al bot serás bloqueado. ${md}`,
+      false,
+      {mentions: [m.sender]}
+    );
+    await this.updateBlockStatus(m.chat, "block");
   }
   return !1;
 }
