ee975f7b1a018c52cff298117c2980b7eec2c329
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-04 15:25:31 -0500

    Actualizar owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -1,10 +1,4 @@
 import fs from 'fs';
-
-const isUserBlockedInGroup = (userId, groupId) => {
-  const blockedUsers = global.db.data.blockedUsers?.[userId];
-  return blockedUsers && blockedUsers.includes(`${userId}@g.us:${groupId}`);
-};
-
 const handler = async (m, { conn, command }) => {
   const why = `Uso correcto\n${command} @usuario`;
   const who = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : null);
@@ -19,12 +13,30 @@ const handler = async (m, { conn, command }) => {
       await conn.updateBlockStatus(who, 'block').then(() => {
         res.push(who);
         if (groupName) {
+          // Guardar información en la base de datos solo si es un grupo
           global.db.data.blockedUsers = global.db.data.blockedUsers || {};
           global.db.data.blockedUsers[who] = global.db.data.blockedUsers[who] || [];
-          if (!global.db.data.blockedUsers[who].includes(`${who}@g.us:${groupName}`)) {
-            global.db.data.blockedUsers[who].push(`${who}@g.us:${groupName}`);
+          if (!global.db.data.blockedUsers[who].includes(groupName)) {
+            global.db.data.blockedUsers[who].push(groupName); // Guarda el nombre del grupo
           }
-          fs.writeFileSync('./lib/databasepro.json', JSON.stringify(global.db.data, null, 2), 'utf-8');
+          fs.writeFileSync('./lib/databasepro.json', JSON.stringify(global.db.data, null, 2), 'utf-8'); // Guarda la base de datos de forma síncrona
+        }
+      });
+      break;
+    case 'unblok':
+    case 'unblock':
+      await conn.updateBlockStatus(who, 'unblock').then(() => {
+        res.push(who);
+        if (groupName) {
+          // Eliminar la información del grupo de la base de datos solo si es un grupo
+          if (global.db.data.blockedUsers && global.db.data.blockedUsers[who]) {
+            const index = global.db.data.blockedUsers[who].indexOf(groupName);
+            if (index !== -1) {
+              global.db.data.blockedUsers[who].splice(index, 1);
+              if (global.db.data.blockedUsers[who].length === 0) delete global.db.data.blockedUsers[who];
+              fs.writeFileSync('./lib/databasepro.json', JSON.stringify(global.db.data, null, 2), 'utf-8'); // Guarda la base de datos de forma síncrona
+            }
+          }
         }
       });
       break;
@@ -42,9 +54,9 @@ const handler = async (m, { conn, command }) => {
   }
 };
 
-handler.help = ['block (@usuario)'];
+handler.help = ['block/unblock (@usuario)'];
 handler.tags = ['owner'];
-handler.command = /^(blok|block)$/i;
+handler.command = /^(block|unblock)$/i;
 handler.rowner = true;
 
 export default handler;
