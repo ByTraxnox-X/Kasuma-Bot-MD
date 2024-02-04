852c5bc0b4a7af57bdace555ce280e24a81358aa
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-03 21:48:57 -0500

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -3,18 +3,19 @@ import fs from 'fs';
 const handler = async (m, { conn }) => {
   try {
     const data = await conn.fetchBlocklist();
+
     if (data.length === 0) throw 'No hay números bloqueados';
 
     let txt = `*Lista de bloqueados*\n\n*Total :* ${data.length}\n\n`;
 
     for (let i of data) {
-      let groupNames = global.db.data.blockedUsers && global.db.data.blockedUsers[i] ? global.db.data.blockedUsers[i].join(', ') : '';
+      const groupNames = global.db.data.blockedUsers && global.db.data.blockedUsers[i] ? global.db.data.blockedUsers[i].join(', ') : '';
       txt += `@${i.split("@")[0]} - grupos: ${groupNames}\n`;
     }
 
     conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     throw 'Error al obtener la lista de bloqueados';
   }
 };
