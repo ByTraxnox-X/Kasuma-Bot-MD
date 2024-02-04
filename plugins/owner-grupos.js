847eea5e1c5ff1179eac454245a224da76c60c93
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-04 14:37:55 -0500

    Actualizar owner-grupos.js

--- plugins/owner-grupos.js
+++ plugins/owner-grupos.js
@@ -1,45 +1,28 @@
 import fs from 'fs';
 
-const isUserBlockedInGroup = (userId, groupName) => {
-  return (
-    global.db.data.blockedUsers &&
-    global.db.data.blockedUsers[userId] &&
-    global.db.data.blockedUsers[userId].includes(groupName)
-  );
-};
-
 const handler = async (m, { conn }) => {
   let txt = 'Lista de grupos\n';
-  let blockedTxt = '';
 
   // Obtener la lista de grupos
   const groups = Object.values(await conn.groupFetchAllParticipating());
 
-  for (const [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) {
-    const groupName = await conn.getName(jid);
-    const groupId = jid;
+  for (const group of groups) {
+    const groupName = group.subject || group.id;
 
-    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${groupId} [${chat?.metadata?.read_only ? 'No participa' : 'Participa'}]`;
+    txt += `\nNombre del grupo: ${groupName}\nID del grupo: ${group.id} [${group.metadata?.read_only ? 'No participa' : 'Participa'}`;
 
-    // Verificar si el usuario y el bot están en el mismo grupo
-    const isUserInGroup = groups.find(group => group.id === jid);
-    if (isUserInGroup) {
-      // Verificar y mostrar usuarios bloqueados en este grupo
-      if (global.db.data.blockedUsers) {
-        const blockedUsers = global.db.data.blockedUsers[conn.user.jid];
-        const blockedUsersInGroup = blockedUsers?.filter(user => isUserBlockedInGroup(user, jid));
+    // Verificar y mostrar usuarios bloqueados en este grupo
+    if (global.db.data.blockedUsers) {
+      const blockedUsers = global.db.data.blockedUsers[conn.user.jid];
+      const blockedUsersInGroup = blockedUsers?.filter(user => user.endsWith('@g.us') && group.participants.find(p => p.jid === user));
 
-        if (blockedUsersInGroup && blockedUsersInGroup.length > 0) {
-          blockedTxt += `\nBloqueados en ${groupName}: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
-        }
+      if (blockedUsersInGroup && blockedUsersInGroup.length > 0) {
+        txt += `, Bloqueados: ${blockedUsersInGroup.map(user => `@${user.split('@')[0]}`).join(', ')}`;
       }
     }
 
-    txt += '\n\n';
+    txt += ']\n\n';
   }
-
-  // Agregar la sección de usuarios bloqueados al final del mensaje
-  txt += blockedTxt;
 
   conn.reply(m.chat, txt.trim(), m);
 };
