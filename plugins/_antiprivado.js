2fb022391197f948cff7aed2a9463663a8559047
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-04 05:46:16 +0100

    API

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,38 +1,14 @@
-const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creador|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i;
-
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, usedPrefix, command }) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  
-  const regex = new RegExp(`^${comandos.source}$`, 'i');
-
-  if (regex.test(m.text.toLowerCase().trim())) return !0;
-
-  let chat, user, bot, mensaje;
-  chat = global.db.data.chats[m.chat];
-  user = global.db.data.users[m.sender];
-  bot = global.db.data.settings[conn.user.jid] || {};
-
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+  if (m.isBaileys && m.fromMe) return !0
+  if (m.isGroup) return !1
+  if (!m.message) return !0
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA')) return !0
+  let chat = global.db.data.chats[m.chat]
+  let bot = global.db.data.settings[this.user.jid] || {}
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    if (user.counterPrivate === 0) {
-      mensaje = `*@${m.sender.split`@`[0]} ESTÁ PROHIBIDO ESCRIBIR AL PRIVADO*\n\n⚠️ \`\`\`PRIMERA ADVERTENCIA\`\`\` ⚠️`;
-      await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] });
-
-    } else if (user.counterPrivate === 1) {
-      let grupos = ['nn', 'nnn', 'nnnt', 'nnntt', 'nnnttt'].getRandom();
-      mensaje = `*@${m.sender.split`@`[0]} YA SE MENCIONÓ QUE NO PUEDE ESCRIBIR AL PRIVADO. 🫤*\n\n⬇️ *PUEDE UNIRSE A ESTE GRUPO OFICIAL*\n${wagp}\n\n*SI VUELVE A ESCRIBIR SERÁ BLOQUEADO(A)*\n⚠️ \`\`\`SEGUNDA ADVERTENCIA\`\`\` ⚠️`;
-      await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] });
-
-    } else if (user.counterPrivate === 2) {
-      mensaje = `*@${m.sender.split`@`[0]} SERÁ BLOQUEADO(A). 👋 SE MENCIONÓ ANTES QUE NO PODÍA ESCRIBIR AL PRIVADO.*\n\n⚠️ \`\`\`TERCERA ADVERTENCIA\`\`\` ⚠️`;
-      await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] });
-
-      user.counterPrivate = -1;
-      await conn.updateBlockStatus(m.chat, 'block');
-    }
-    user.counterPrivate++;
-  } else if (chat.antiPrivate) return; //m.reply('')
-
-  return !1;
-}
+  await m.reply(`*hola @${m.sender.split`@`[0]}, hablar con el bot en el privado es ilegal por tanto seras bloqueado. Si desea usar el bot entre en este grupo:* 
+  ${wagp}`, false, { mentions: [m.sender] })
+  await this.updateBlockStatus(m.chat, 'block')}
+  return !1
+  }
+  
