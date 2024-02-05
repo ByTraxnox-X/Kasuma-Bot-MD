db0cce981e54ba043060b0601303b31d14e50aab
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-05 09:51:00 -0500

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -1,25 +1,22 @@
-const handler = async (m, { conn }) => {
-  try {
-    const data = await conn.fetchBlocklist();
-    let txt = `Lista de bloqueados*\n\nTotal :* ${data.length}\n\n\n`;
-
-    for (let i of data) {
-      let blockedUser = global.db.data && global.db.data.blockedUsers && global.db.data.blockedUsers[i] ? global.db.data.blockedUsers[i] : [];
-      let uniqueGroups = [...new Set(blockedUser)]; // Eliminar duplicados
-      let groupNames = await Promise.all(uniqueGroups.map(groupId => conn.getName(groupId) || groupId));
-      txt += `@${i.split("@")[0]} - grupos: ${groupNames.join(', ')}\n`;
-    }
-
-    conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) });
-  } catch (err) {
-    console.log(err);
-    throw 'No hay números bloqueados';
-  }
+let handler = async (m, { conn }) => {
+	
+	await conn.fetchBlocklist().then(async data => {
+		let txt = `*Lista de bloqueados*\n\n*Total :* ${data.length}\n\n\n`
+		for (let i of data) {
+			txt += `@${i.split("@")[0]}\n`
+		}
+		txt += ""
+		return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
+	}).catch(err => {
+		console.log(err);
+		throw 'No hay números bloqueados'
+	})
 }
 
-handler.help = ['bloqueados'];
-handler.tags = ['owner'];
-handler.command = ['bloqueados', 'listblock'];
-handler.rowner = true;
+handler.help = ['bloqueados']
+handler.tags = ['owner']
+handler.command = ['bloqueados', 'listblock'] 
 
-export default handler;
+handler.rowner = true
+
+export default handler
