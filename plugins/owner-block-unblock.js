60ffe647f52b9986c07b83cefe6181f3bc6f8b93
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-05 09:58:02 -0500

    Actualizar owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -1,30 +1,39 @@
 const handler = async (m, {text, conn, usedPrefix, command}) => {
   const why = `uso correcto\n${usedPrefix + command} @${m.sender.split('@')[0]}`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
-  const chat = conn.chats.get(m.chat);
-  
+
   if (!who) conn.reply(m.chat, why, m, {mentions: [m.sender]});
   const res = [];
 
   switch (command) {
     case 'blok': case 'block':
       if (who) {
-        const group = conn.chats.get(who);
-        const groupName = group ? group.name : 'Unknown Group';
-        await conn.updateBlockStatus(who, 'block').then(() => {
-          res.push(who);
-        });
-        conn.reply(m.chat, `*se completo la operacion (${command}) en el grupo ${groupName} con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+        try {
+          const chat = await conn.getChat(who);
+          const groupName = chat ? chat.name : 'Unknown Group';
+          await conn.updateBlockStatus(who, 'block').then(() => {
+            res.push(who);
+          });
+          conn.reply(m.chat, `*se completo la operacion (${command}) en el grupo ${groupName} con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+        } catch (error) {
+          console.error(error);
+          conn.reply(m.chat, `Error al obtener información del grupo.`, m);
+        }
       } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
       break;
     case 'unblok': case 'unblock':
       if (who) {
-        const group = conn.chats.get(who);
-        const groupName = group ? group.name : 'Unknown Group';
-        await conn.updateBlockStatus(who, 'unblock').then(() => {
-          res.push(who);
-        });
-        conn.reply(m.chat, `*se completo la operacion (${command}) en el grupo ${groupName} con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+        try {
+          const chat = await conn.getChat(who);
+          const groupName = chat ? chat.name : 'Unknown Group';
+          await conn.updateBlockStatus(who, 'unblock').then(() => {
+            res.push(who);
+          });
+          conn.reply(m.chat, `*se completo la operacion (${command}) en el grupo ${groupName} con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
+        } catch (error) {
+          console.error(error);
+          conn.reply(m.chat, `Error al obtener información del grupo.`, m);
+        }
       } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
       break;
   }
