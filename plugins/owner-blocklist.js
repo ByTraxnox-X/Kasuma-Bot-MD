0eef135cb9c3bb06d118fca38b9660d997c881e9
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-05 10:26:23 -0500

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -5,12 +5,31 @@ const handler = async (m, { conn }) => {
 
     const fetchGroupNames = async (user) => {
       try {
-        const chat = await conn.getChat(user);
+        const chat = await conn.getChatById(user);
         return chat ? chat.name : 'Unknown Group';
       } catch (error) {
         console.error(error);
         return 'Unknown Group';
       }
+    };
+
+    const updateDatabase = () => {
+      fs.writeFileSync('./lib/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8');
+    };
+
+    const unblockUser = async (user) => {
+      const groupName = await fetchGroupNames(user);
+      await conn.updateBlockStatus(user, 'unblock').then(() => {
+        // Eliminar la información del grupo de la base de datos solo si es un grupo
+        if (global.db.data.blockedUsers && global.db.data.blockedUsers[user]) {
+          const index = global.db.data.blockedUsers[user].indexOf(groupName);
+          if (index !== -1) {
+            global.db.data.blockedUsers[user].splice(index, 1);
+            if (global.db.data.blockedUsers[user].length === 0) delete global.db.data.blockedUsers[user];
+            updateDatabase();
+          }
+        }
+      });
     };
 
     const groupNamesPromises = blockedUsers.map(async (user) => {
