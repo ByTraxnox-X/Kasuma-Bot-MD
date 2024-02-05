b65415804eae3de68cfd5f487d14d060dd2abb5c
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-05 09:56:38 -0500

    Actualizar owner-block-unblock.js

--- plugins/owner-block-unblock.js
+++ plugins/owner-block-unblock.js
@@ -1,31 +1,38 @@
 const handler = async (m, {text, conn, usedPrefix, command}) => {
   const why = `uso correcto\n${usedPrefix + command} @${m.sender.split('@')[0]}`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
+  const chat = conn.chats.get(m.chat);
+  
   if (!who) conn.reply(m.chat, why, m, {mentions: [m.sender]});
   const res = [];
+
   switch (command) {
     case 'blok': case 'block':
       if (who) {
+        const group = conn.chats.get(who);
+        const groupName = group ? group.name : 'Unknown Group';
         await conn.updateBlockStatus(who, 'block').then(() => {
           res.push(who);
         });
+        conn.reply(m.chat, `*se completo la operacion (${command}) en el grupo ${groupName} con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
       } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
       break;
     case 'unblok': case 'unblock':
       if (who) {
+        const group = conn.chats.get(who);
+        const groupName = group ? group.name : 'Unknown Group';
         await conn.updateBlockStatus(who, 'unblock').then(() => {
           res.push(who);
         });
+        conn.reply(m.chat, `*se completo la operacion (${command}) en el grupo ${groupName} con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
       } else conn.reply(m.chat, why, m, {mentions: [m.sender]});
       break;
   }
-  if (res[0]) conn.reply(m.chat, `*se completo la operacion (${command}) con ${res ? `${res.map((v) => '@' + v.split('@')[0])}` : ''}*`, m, {mentions: res});
 };
 
-handler.help = ['block/unblock (@user)']
-handler.tags = ['owner']
+handler.help = ['block/unblock (@user)'];
+handler.tags = ['owner'];
 handler.command = /^(block|unblock)$/i;
-handler.rowner = true
+handler.rowner = true;
 
-export default handler
-
+export default handler;
