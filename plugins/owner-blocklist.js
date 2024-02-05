55caa3d0f8cbbbf9952909884d667b3d905c94e7
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-05 09:53:46 -0500

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -1,22 +1,20 @@
 let handler = async (m, { conn }) => {
-	
-	await conn.fetchBlocklist().then(async data => {
-		let txt = `*Lista de bloqueados*\n\n*Total :* ${data.length}\n\n\n`
-		for (let i of data) {
-			txt += `@${i.split("@")[0]}\n`
-		}
-		txt += ""
-		return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
-	}).catch(err => {
-		console.log(err);
-		throw 'No hay números bloqueados'
-	})
+  const blockedUsers = await conn.fetchBlocklist();
+  let txt = `*Lista de bloqueados*\n\n*Total :* ${blockedUsers.length}\n\n\n`;
+
+  for (let user of blockedUsers) {
+    const group = conn.chats.get(user);
+    const groupName = group ? group.name : 'Unknown Group';
+    txt += `@${user.split("@")[0]} en ${groupName}\n`;
+  }
+
+  txt += "";
+  return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) });
 }
 
-handler.help = ['bloqueados']
-handler.tags = ['owner']
-handler.command = ['bloqueados', 'listblock'] 
+handler.help = ['bloqueados'];
+handler.tags = ['owner'];
+handler.command = ['bloqueados', 'listblock'];
+handler.rowner = true;
 
-handler.rowner = true
-
-export default handler
+export default handler;
