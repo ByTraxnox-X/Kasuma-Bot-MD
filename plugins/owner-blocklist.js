bec4f8869b5cf1918f464cd0138f283f982c219e
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-05 10:18:57 -0500

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn }) => {
+const handler = async (m, { conn }) => {
   try {
     const blockedUsers = await conn.fetchBlocklist();
     let txt = `*Lista de bloqueados*\n\n*Total :* ${blockedUsers.length}\n\n\n`;
@@ -13,6 +13,25 @@ let handler = async (m, { conn }) => {
       }
     };
 
+    const updateDatabase = () => {
+      fs.writeFileSync('./lib/database.json', JSON.stringify(global.db.data, null, 2), 'utf-8');
+    };
+
+    const unblockUser = async (user) => {
+      const groupName = await fetchGroupNames(user);
+      await conn.updateBlockStatus(user, 'unblock').then(() => {
+        // Eliminar la información del grupo de la base de datos solo si es un grupo
+        if (global.db.data.blockedUsers && global.db.data.blockedUsers[user]) {
+          const index = global.db.data.blockedUsers[user].indexOf(groupName);
+          if (index !== -1) {
+            global.db.data.blockedUsers[user].splice(index, 1);
+            if (global.db.data.blockedUsers[user].length === 0) delete global.db.data.blockedUsers[user];
+            updateDatabase();
+          }
+        }
+      });
+    };
+
     const groupNamesPromises = blockedUsers.map(async (user) => {
       const groupName = await fetchGroupNames(user);
       return `@${user.split("@")[0]} en ${groupName}`;
@@ -22,12 +41,12 @@ let handler = async (m, { conn }) => {
     txt += groupNames.join('\n');
 
     txt += "";
-    return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) });
+    conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) });
   } catch (error) {
     console.error(error);
     throw 'No hay números bloqueados';
   }
-}
+};
 
 handler.help = ['bloqueados'];
 handler.tags = ['owner'];
@@ -35,4 +54,3 @@ handler.command = ['bloqueados', 'listblock'];
 handler.rowner = true;
 
 export default handler;
-
