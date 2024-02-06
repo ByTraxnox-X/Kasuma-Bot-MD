5206faa768824d3879437f3eda3a6cf9190c4c05
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-06 20:51:31 +0100

    KASUMA 2.0

---
+++ plugins/dl-pinterest.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, args }) => {
+    try {
+        const apiUrl = `${apikasu}/api/search/pinterest?text=${encodeURIComponent(args[0])}&apikey=${apikeykasu}`;
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.status && data.result.length > 0) {
+            m.react(rwait);
+
+            const firstThreeImages = data.result.slice(0, 3);
+
+            for (const imageUrl of firstThreeImages) {
+                m.reply('Enviando imagen...');
+                await conn.sendFile(m.chat, imageUrl, 'imagen.jpg', '', m);
+            }
+
+            m.react(done);
+        } else {
+            throw 'No se pudo obtener imágenes de Pinterest.';
+        }
+    } catch (error) {
+        throw `Ocurrió un error al procesar la solicitud: ${error}`;
+    }
+};
+
+handler.help = ['pinterest'];
+handler.tags = ['dl'];
+handler.command = /^pinterest$/i;
+
+export default handler;
