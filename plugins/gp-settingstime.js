ecfdf7f882dfe9f2e67379d8bb87cde17763fdc2
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-07 07:57:24 -0500

    Actualizar gp-settingstime.js

--- plugins/gp-settingstime.js
+++ plugins/gp-settingstime.js
@@ -4,16 +4,14 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
     throw false;
   }
 
-  if (args.length !== 4 || args[1].toLowerCase() !== 'auto') {
-    m.reply(`Formato incorrecto. Uso: ${usedPrefix + command} auto <hora_inicio AM/PM>|<hora_fin AM/PM>`);
+  if (args.length !== 3 || args[1].toLowerCase() !== 'auto') {
+    m.reply(`Formato incorrecto. Uso: ${usedPrefix + command} auto <hora_inicio|hora_fin>`);
     throw false;
   }
 
-  let [startHour, startMinute, startPeriod] = args[2].match(/(\d+):(\d+)([APMapm]{2})/).slice(1);
-  let [endHour, endMinute, endPeriod] = args[3].match(/(\d+):(\d+)([APMapm]{2})/).slice(1);
-
-  startHour = (startPeriod.toLowerCase() === 'pm' && startHour < 12) ? Number(startHour) + 12 : Number(startHour) % 12;
-  endHour = (endPeriod.toLowerCase() === 'pm' && endHour < 12) ? Number(endHour) + 12 : Number(endHour) % 12;
+  let [start, end] = args[2].split('|');
+  let [startHour, startMinute] = start.split(':').map(Number);
+  let [endHour, endMinute] = end.split(':').map(Number);
 
   let currentTime = new Date().getHours() * 60 + new Date().getMinutes();
   let startTime = startHour * 60 + startMinute;
@@ -21,14 +19,14 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
 
   if (currentTime >= startTime && currentTime < endTime) {
     await conn.groupSettingUpdate(m.chat, 'not_announcement');
-    m.reply(`Grupo cerrado automáticamente. Próximo evento de ${args[2]} a ${args[3]}`);
+    m.reply(`Grupo cerrado automáticamente. Próximo evento de ${start} a ${end}`);
   } else {
     await conn.groupSettingUpdate(m.chat, 'announcement');
-    m.reply(`Grupo abierto automáticamente. Próximo evento de ${args[2]} a ${args[3]}`);
+    m.reply(`Grupo abierto automáticamente. Próximo evento de ${start} a ${end}`);
   }
 };
 
-handler.help = ['grouptime auto <hora_inicio AM/PM>|<hora_fin AM/PM>'];
+handler.help = ['grouptime auto <hora_inicio|hora_fin>'];
 handler.tags = ['group'];
 handler.command = /^(grouptime|gctime)$/i;
 
