dd6b75f4c49fa9887b12e5592f9fa63f6263176f
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-07 13:18:20 -0500

    Actualizar plugins

---
+++ plugins/game-bandera.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+let timeout = 120000
+let poin = 4999
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, 'Aún quedan preguntas sin respuesta en este chat.', conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakbendera2.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
+Se acabó el tiempo *${(timeout / 1000).toFixed(2)} segundo*
+usar ${usedPrefix}pistabandera
+Premio: ${poin} XP
+    `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.img, '', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `¡Se acabó el tiempo!\nLa respuesta es *${json.name}*`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+}
+handler.help = ['bandera']
+handler.tags = ['game']
+handler.command = /^bandera/i
+
+export default handler
