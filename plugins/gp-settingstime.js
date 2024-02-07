1d7ca2b8581da55b4c8fe9bebd73974bc963831b
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-07 08:39:48 -0500

    Actualizar gp-settingstime.js

--- plugins/gp-settingstime.js
+++ plugins/gp-settingstime.js
@@ -1,46 +1,55 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    try {
-        let isAuto = args[0].toLowerCase() === 'auto';
-        let isOpen = args[1].toLowerCase() === 'abrir';
-        let isClose = args[1].toLowerCase() === 'cerrar';
+let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) => {
+  if (!(isAdmin || isOwner)) {
+	  global.dfail('admin', m, conn)
+          throw false
+  }
+  let isClose = {
+	  'open': 'not_announcement',
+	  'buka': 'not_announcement',
+      'on': 'not_announcement',
+	  '1': 'not_announcement',
+	  'close': 'announcement',
+	  'tutup': 'announcement',
+      'off': 'announcement',
+      '0': 'announcement',
+  }[(args[0] || '')]
+  if (isClose === undefined) {
+	  let caption = `
+*FORMATO ERRONEO!!*
+ 
+ ${usedPrefix + command} open 1*
+ ${usedPrefix + command} close 1*
+ *Ejemplo de uso:* *${usedPrefix + command} close 1* 
+ Para que el grupo este cerrado una hora.*
 
-        if (!(isAuto || isOpen || isClose))
-            throw `
-*Elija una opción:*
-  *${usedPrefix + command} abrir*
-  *${usedPrefix + command} cerrar*
-  *${usedPrefix + command} auto <hora_inicio|hora_fin>*
-`.trim();
+`
+      m.reply(caption)
+	  throw false
+  }
+  let timeoutset = 86400000 * args[1] / 24
+  await conn.groupSettingUpdate(m.chat, isClose).then(async _=> {
+	  m.reply(` *Grupo ${isClose == 'announcement' ? 'cerrado' : 'abierto'} ${args[1] ? `durante*${clockString(timeoutset)}*` : ''}`)
+  })
+  if (args[1]) {
+	 setTimeout(async () => {
+            await conn.groupSettingUpdate(m.chat, `${isClose == 'announcement' ? 'not_announcement' : 'announcement'}`).then(async _=>{
+		    conn.reply(m.chat, `${isClose == 'not_announcement' ? '*El grupo ha sido cerrado, ahora solo los administradores pueden enviar mensajes!*' : '*El grupo se ha abierto, ahora todos los miembros pueden enviar mensajes!*'}!`)
+	    })
+        }, timeoutset)
+  }
+  }
+handler.help = ['grouptime <open/close> <horas>']
+handler.tags = ['group']
+handler.command = /^(grouptime|gctime)$/i
 
-        if (isAuto) {
-            let [start, end] = args[2].match(/\b\d{1,2}:\d{2}[APMapm]{2}\b/g) || [];
-            if (!start || !end) {
-                m.reply("Formato de hora incorrecto. Utiliza un formato como 8:30AM|9:30PM");
-                throw false;
-            }
+handler.botAdmin = true
+handler.group = true 
+export default handler
 
-            const setGroupSettingAtTime = async (isOpen, time) => {
-                await conn.groupSettingUpdate(m.chat, isOpen ? 'not_announcement' : 'announcement');
-                m.reply(`Grupo ${isOpen ? 'abierto' : 'cerrado'} automáticamente. Próximo evento a las ${time}`);
-            };
-
-            setGroupSettingAtTime(isOpen, start); // Abrir o cerrar grupo
-            setGroupSettingAtTime(!isOpen, end); // Cerrar o abrir grupo
-        } else {
-            let isSetting = isOpen ? 'not_announcement' : 'announcement';
-            await conn.groupSettingUpdate(m.chat, isSetting);
-            m.reply(`Grupo ${isOpen ? 'abierto' : 'cerrado'} manualmente.`);
-        }
-    } catch (error) {
-        console.error(error);
-        // Manejar cualquier error aquí
-    }
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
-
-handler.help = ['grupotime abrir/cerrar/auto <hora_inicio|hora_fin>'];
-handler.tags = ['group'];
-handler.command = ['timegrupo', 'grupotime'];
-handler.admin = true;
-handler.botAdmin = true;
-
-export default handler;
