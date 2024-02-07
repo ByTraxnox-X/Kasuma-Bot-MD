230a3a7a7aef9ce33abcf3bfb1baecc453e90d41
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-07 08:11:15 -0500

    Actualizar gp-settingstime.js

--- plugins/gp-settingstime.js
+++ plugins/gp-settingstime.js
@@ -4,12 +4,35 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
     throw false;
   }
 
-  if (args.length !== 3 || args[1].toLowerCase() !== 'auto') {
-    m.reply(`Formato incorrecto. Uso: ${usedPrefix + command} auto <hora1|hora2>`);
+  let isClose = {
+    'open': 'not_announcement',
+    'buka': 'not_announcement',
+    'on': 'not_announcement',
+    '1': 'not_announcement',
+    'close': 'announcement',
+    'tutup': 'announcement',
+    'off': 'announcement',
+    '0': 'announcement',
+  }[(args[0] || '')];
+
+  if (isClose === undefined) {
+    let caption = `
+*FORMATO ERRONEO!!*
+ 
+ ${usedPrefix + command} open 1*
+ ${usedPrefix + command} close 1*
+ *Ejemplo de uso:* *${usedPrefix + command} close 1* 
+ Para que el grupo esté cerrado una hora.*
+
+`;
+    m.reply(caption);
     throw false;
   }
 
-  let [start, end] = args[2].split('|');
+  let [start, end] = args[1].split('|');
+  let [startHour, startMinute] = start.split(':').map(Number);
+  let [endHour, endMinute] = end.split(':').map(Number);
+
   let now = new Date();
   let startTime = new Date(now.toDateString() + ' ' + start);
   let endTime = new Date(now.toDateString() + ' ' + end);
@@ -19,7 +42,7 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
   m.reply(`Grupo ${isTimeInRange ? 'abierto' : 'cerrado'} automáticamente. Próximo evento de ${start} a ${end}`);
 };
 
-handler.help = ['grouptime auto <hora1|hora2>'];
+handler.help = ['grouptime <open/close> <hora_inicio|hora_fin>'];
 handler.tags = ['group'];
 handler.command = /^(grouptime|gctime)$/i;
 
