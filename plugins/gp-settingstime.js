c3f58f46536818afe13f8bf93b6ed03265e0b677
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-07 12:00:48 -0500

    Actualizar gp-settingstime.js

--- plugins/gp-settingstime.js
+++ plugins/gp-settingstime.js
@@ -1,63 +1,55 @@
 let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) => {
   if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
+	  global.dfail('admin', m, conn)
+          throw false
   }
-
   let isClose = {
-    'abrirgrupoen': 'not_announcement',
-    'cerrargrupoen': 'announcement',
-  }[(args[0] || '').toLowerCase()];
-
+	  'open': 'not_announcement',
+	  'abrirgrupoen': 'not_announcement',
+      'on': 'not_announcement',
+	  '1': 'not_announcement',
+	  'close': 'announcement',
+	  'cerrargrupoen': 'announcement',
+      'off': 'announcement',
+      '0': 'announcement',
+  }[(args[0] || '')]
   if (isClose === undefined) {
-    let caption = `
+	  let caption = `
 *FORMATO ERRONEO!!*
+ 
+ ${usedPrefix + command} 1*
+ ${usedPrefix + command} 1*
+ *Ejemplo de uso:* *${usedPrefix + command} close 1* 
+ Para que el grupo este cerrado una hora.*
 
-Uso:
-  *${usedPrefix}abrirgrupoen <duración_en_horas>*
-  *${usedPrefix}cerrargrupoen <duración_en_horas>*
-  
-Ejemplo de uso:
-  *${usedPrefix}abrirgrupoen 1*
-  *${usedPrefix}cerrargrupoen 1*
-  
-Para abrir o cerrar el grupo por una duración específica.
-`;
-
-    m.reply(caption);
-    throw false;
+`
+      m.reply(caption)
+	  throw false
   }
-
-  let timeoutset = 86400000 * args[1] / 24;
-
-  await conn.groupSettingUpdate(m.chat, isClose).then(async (_) => {
-    m.reply(`Grupo ${isClose == 'announcement' ? 'cerrado' : 'abierto'} ${args[1] ? `durante*${clockString(timeoutset)}*` : ''}`);
-  });
-
+  let timeoutset = 86400000 * args[1] / 24
+  await conn.groupSettingUpdate(m.chat, isClose).then(async _=> {
+	  m.reply(` *Grupo ${isClose == 'announcement' ? 'cerrado' : 'abierto'} ${args[1] ? `durante*${clockString(timeoutset)}*` : ''}`)
+  })
   if (args[1]) {
-    setTimeout(async () => {
-      await conn.groupSettingUpdate(m.chat, `${isClose == 'announcement' ? 'not_announcement' : 'announcement'}`).then(async (_) => {
-        conn.reply(m.chat, `${isClose == 'not_announcement' ? '*El grupo ha sido cerrado, ahora solo los administradores pueden enviar mensajes!*' : '*El grupo se ha abierto, ahora todos los miembros pueden enviar mensajes!*'}!`);
-      });
-    }, timeoutset);
+	 setTimeout(async () => {
+            await conn.groupSettingUpdate(m.chat, `${isClose == 'announcement' ? 'not_announcement' : 'announcement'}`).then(async _=>{
+		    conn.reply(m.chat, `${isClose == 'not_announcement' ? '*El grupo ha sido cerrado, ahora solo los administradores pueden enviar mensajes!*' : '*El grupo se ha abierto, ahora todos los miembros pueden enviar mensajes!*'}!`)
+	    })
+        }, timeoutset)
   }
-};
+  }
+handler.help = ['cerrargrupoen <horas>|abrirgrupoen <horas>']
+handler.tags = ['group']
+handler.command = /^(cerrargrupoen|abrirgrupoen)$/i
 
-handler.help = ['grouptime <abrirgrupoen/cerrargrupoen> <horas>'];
-handler.tags = ['group'];
-handler.command = /^(grouptime|gctime)$/i;
-
-handler.botAdmin = true;
-handler.group = true;
-
-export default handler;
+handler.botAdmin = true
+handler.group = true 
+export default handler
 
 function clockString(ms) {
-  let h = Math.floor(ms / 3600000);
-  let m = Math.floor(ms / 60000) % 60;
-  let s = Math.floor(ms / 1000) % 60;
-
-  console.log({ ms, h, m, s });
-
-  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
