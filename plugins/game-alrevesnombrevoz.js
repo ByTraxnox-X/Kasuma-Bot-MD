1ae57e866101c80c0a7c4d2054a29dbef5df5fad
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-08 10:30:43 -0500

    Actualizar plugins

---
+++ plugins/game-alrevesnombrevoz.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { text }) => {
+  if (!text) {
+    throw 'Por favor, escribe tu nombre para jugar';
+  }
+
+  try {
+    const reversedName = text.split('').reverse().join('');
+
+    const apiUrl = `https://apikasu.onrender.com/api/soundoftext?text=${encodeURIComponent(reversedName)}&lang=es-ES&apikey=GuillermoDevelop`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.status && data.result && data.result.length > 0) {
+      const audioUrl = data.result[0];
+      const audioBuffer = await fetch(audioUrl).then(res => res.buffer());
+
+      await m.replyAudio(audioBuffer, null, { ptt: true });
+    } else {
+      throw 'No se pudo obtener el audio de la API';
+    }
+  } catch (error) {
+    throw `Ocurrió un error al generar el audio: ${error}`;
+  }
+};
+
+handler.command = ['nombrealrevezvoz'];
+handler.help = ['nombrealrevezvoz'];
+handler.tags = ['game'];
+export default handler;
