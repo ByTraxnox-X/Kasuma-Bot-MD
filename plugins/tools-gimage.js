4fbc10ddc288f26819861cfff5bcc1d141969a5e
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-08 03:29:51 +0100

    KASUMA 2.0

--- plugins/tools-gimage.js
+++ plugins/tools-gimage.js
@@ -1,14 +1,39 @@
+import fetch from 'node-fetch';
 
-import fg from 'api-dylux'
-let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
-  if (!text) throw `Ingrese la imagen que quiere buscar \n\nEjemplo: *${usedPrefix + command}* Billie Eilish`
-  let res = await fg.googleImage(text)
-  conn.sendFile(m.chat, res.getRandom(), 'img.png', `
-Resultado de: *${text}*`.trim(), m)
-}
-handler.help = ['imagen']
-handler.tags = ['img']
-handler.command = /^(img|image|gimage|imagen)$/i
-handler.diamond = true
+const handler = async (m, { conn, text }) => {
+  if (!text) {
+    throw 'Por favor, proporciona un texto';
+  }
 
-export default handler
+  try {
+    conn.sendPresenceUpdate('composing', m.chat);
+
+    const apiUrl = `https://apikasu.onrender.com/api/search/googleimg?text=${encodeURIComponent(text)}&apikey=SebastianDevelop`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (response.ok && data.status && data.result && data.result.length >= 2) {
+      const [firstImage, secondImage] = data.result.slice(0, 2);
+      
+      const firstImageResponse = await fetch(firstImage);
+      const firstImageBuffer = await firstImageResponse.buffer();
+
+      const secondImageResponse = await fetch(secondImage);
+      const secondImageBuffer = await secondImageResponse.buffer();
+
+      conn.sendFile(m.chat, firstImageBuffer, 'imagen1.jpg', '', m);
+      conn.sendFile(m.chat, secondImageBuffer, 'imagen2.jpg', '', m);
+    } else {
+      throw 'No se pudo obtener una respuesta válida';
+    }
+  } catch (error) {
+    throw `Ocurrió un error: ${error}`;
+  }
+};
+
+handler.help = ['imagen'];
+handler.tags = ['ai'];
+handler.command = ["image", "img", "imagen"]
+
+export default handler;
