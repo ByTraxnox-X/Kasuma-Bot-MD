71657127bc2b4836672dcd1583a3b0e4d5c9c15e
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-08 19:36:02 -0500

    Actualizar timegp.js

--- plugins/timegp.js
+++ plugins/timegp.js
@@ -4,11 +4,22 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
     throw false;
   }
 
-  await conn.groupSettingUpdate(m.chat, 'not_announcement');
-  m.reply('*Grupo abierto*');
+  if (args[0] === undefined || isNaN(args[0])) {
+    m.reply('*FORMATO ERRONEO!!*\n\n*Ejemplo de uso:* *' + usedPrefix + command + ' 1*\n*Para cerrar el grupo durante una hora.*');
+    throw false;
+  }
+
+  let timeoutset = 86400000 * args[0] / 24;
+  await conn.groupSettingUpdate(m.chat, 'not_announcement').catch(() => {});
+  m.reply(`*Grupo abierto durante ${args[0]} horas*`);
+
+  setTimeout(async () => {
+    await conn.groupSettingUpdate(m.chat, 'announcement').catch(() => {});
+    conn.reply(m.chat, '*El grupo se ha cerrado, ahora solo los administradores pueden enviar mensajes!*');
+  }, timeoutset);
 };
 
-handler.help = ['abrirgrupoen'];
+handler.help = ['abrirgrupoen <horas>'];
 handler.tags = ['group'];
 handler.command = /^(abrirgrupoen)$/i;
 handler.botAdmin = true;
