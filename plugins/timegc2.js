e8e4b53a93a7ec8acaaef3883968d0b8c165fdfa
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-08 19:43:00 -0500

    Actualizar timegc2.js

--- plugins/timegc2.js
+++ plugins/timegc2.js
@@ -4,22 +4,23 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
     throw false;
   }
 
-  if (args[0] === undefined || isNaN(args[0])) {
-    m.reply('*FORMATO ERRONEO!!*\n\n*Ejemplo de uso:* *' + usedPrefix + command + ' 1*\n*Para cerrar el grupo durante una hora.*');
+  let duration = args[0].match(/(\d+)([mh])/);
+  if (!duration) {
+    m.reply('*FORMATO ERRONEO!!*\n\n*Ejemplo de uso:* *' + usedPrefix + command + ' m30*\n*Para cerrar el grupo durante 30 minutos.*');
     throw false;
   }
 
-  let timeoutset = 86400000 * args[0] / 24;
-  await conn.groupSettingUpdate(m.chat, 'announcement');
-  m.reply(`*Grupo cerrado durante ${args[0]} horas*`);
+  let timeoutset = duration[2] === 'h' ? 3600000 * duration[1] : 60000 * duration[1];
+  await conn.groupSettingUpdate(m.chat, 'announcement').catch(() => {});
+  m.reply(`*Grupo cerrado durante ${duration[1]} ${duration[2] === 'h' ? 'hora(s)' : 'minuto(s)'}*`);
 
   setTimeout(async () => {
-    await conn.groupSettingUpdate(m.chat, 'not_announcement');
+    await conn.groupSettingUpdate(m.chat, 'not_announcement').catch(() => {});
     conn.reply(m.chat, '*El grupo se ha abierto, ahora todos los miembros pueden enviar mensajes!*');
   }, timeoutset);
 };
 
-handler.help = ['cerrargrupoen <horas>'];
+handler.help = ['cerrargrupoen <m/h>'];
 handler.tags = ['group'];
 handler.command = /^(cerrargrupoen)$/i;
 handler.botAdmin = true;
