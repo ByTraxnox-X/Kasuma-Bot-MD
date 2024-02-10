3706d4d63b528a927572dabf76300b54f6cbb0f8
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-10 23:55:10 +0100

    KASUMA 2.0

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,75 +1,65 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js';
-import axios from 'axios';
 
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn }) => {
-    let name = conn.getName(m.sender);
-    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/nHHUm1a.png');
-    let user = global.db.data.users[m.sender];
-
+	  let name = conn.getName(m.sender)
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/nHHUm1a.png')
+    let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-        let { min, xp, max } = xpRange(user.level, global.multiplier);
+        let { min, xp, max } = xpRange(user.level, global.multiplier)
         let txt = `
-*LEVEL*
+ *NIVEL*
 Nombre: *${name}*
 Nivel: *${user.level}*
 XP: *${user.exp - min}/${xp}*
 Rango: *${user.role}*
 
-Te falta *${max - user.exp}* de *XP* para subir de nivel`.trim();
 
-        try {
-            let imgg = await axios.get(`https://api.popcat.xyz/welcomecard`, {
-                params: {
-                    background: 'https://cdn.discordapp.com/attachments/850808002545319957/859359637106065408/bg.png',
-                    text1: name,
-                    text2: 'No%20es%20suficiente%20para%20subir%20de%20nivel',
-                    text3: `XP:%20${user.exp - min}/${xp}`,
-                    avatar: pp,
-                },
-            });
+Te falta *${max - user.exp}* de *XP* para subir de nivel
+`.trim()
+try {
+  let imgg = API(`${apikasu}`, '/api/maker/levelup', {
+    profile: pp,
+    username: name,
+    level: user.level,
+    xp: user.exp - min,
+    apikey: "SebastianDevelop"
+})
 
-            conn.sendFile(m.chat, imgg.data, 'level.jpg', txt, m);
-        } catch (e) {
-            m.reply(txt);
-        }
-    } else {
-        let before = user.level * 1;
+    conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
+} catch (e) {
+    m.reply(txt)
+}
+    }
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+    	user.role = global.rpg.role(user.level).name
 
-        while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++;
-
-        if (before !== user.level) {
-            user.role = global.rpg.role(user.level).name;
-
-            let str = `
+        let str = `
 *LEVEL UP*
-Nombre: *${name}*
 Nivel anterior: *${before}*
-Nivel actual: *${user.level}*
+ Nivel actual: *${user.level}*
 Rango: *${user.role}*
 
-¡Felicidades! Has subido de nivel`.trim();
 
-            try {
-                let img = await axios.get(`https://api.popcat.xyz/welcomecard`, {
-                    params: {
-                        background: 'https://cdn.discordapp.com/attachments/850808002545319957/859359637106065408/bg.png',
-                        text1: name,
-                        text2: 'Ha%20subido%20de%20nivel',
-                        text3: `Nuevo%20nivel: ${user.level}`,
-                        avatar: pp,
-                    },
-                });
-
-                conn.sendFile(m.chat, img.data, 'levelup.jpg', str, m);
-            } catch (e) {
-                m.reply(str);
-            }
+Cuanto más interactúes con los bots, mayor será tu nivel`.trim()
+        try {
+            let img = API(`${apikasu}`, '/api/maker/levelup', { 
+                profile: pp,
+                username: name,
+                level: user.level,
+                xp: user.exp - min,
+                apikey: "SebastianDevelop"
+             })
+      conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+        } catch (e) {
+            m.reply(str)
         }
     }
-};
+}
 
-handler.help = ['nivel'];
-handler.tags = ['econ'];
-handler.command = ['nivel', 'lvl', 'levelup', 'level'];
+handler.help = ['nivel']
+handler.tags = ['econ']
+handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
 
-export default handler;
+export default handler
