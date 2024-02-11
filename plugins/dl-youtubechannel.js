d4bfb6383a94598eab5d64d6d2cd942af77a3864
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-11 05:22:41 +0100

    KASUMA 2.0

--- plugins/dl-youtubechannel.js
+++ plugins/dl-youtubechannel.js
@@ -20,8 +20,7 @@ const handler = async (m, { conn, text }) => {
       msg += `*Descripción:* ${channel.channel_about}\n`;
       msg += `*Fecha de Creación:* ${channel.channel_created}\n`;
 
-      await conn.sendFile(m.chat, channel.channel_picture.medium.url, 'channel_image.jpg', '', m);
-      await conn.sendMessage(m.chat, { text: msg }, { quoted: m });
+      await conn.sendMessage(m.chat, channel.channel_picture.medium.url, 'channel_image.jpg', { text: msg }, { quoted: m });
     } else {
       throw 'No se encontraron resultados para el canal de YouTube proporcionado.';
     }
