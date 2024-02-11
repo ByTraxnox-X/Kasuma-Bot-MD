d3f3a622b0bf40404f053c9a4d71b92f3d7a3f46
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-11 03:14:12 +0100

    KASUMA 2.0

---
+++ plugins/dl-youtubechannel.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text }) => {
+  if (!text) {
+    throw 'Por favor, proporciona el nombre del canal de YouTube.';
+  }
+
+  try {
+    conn.sendPresenceUpdate('composing', m.chat);
+
+    const apiUrl = `${apikasu}/api/search/youtubechannel?channel=${encodeURIComponent(text)}&apikey=${apikeykasu}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.status && data.result && data.result.length > 0) {
+      const channel = data.result[0];
+      let msg = `*Nombre del Canal:* ${channel.channel_name}\n`;
+      msg += `*ID del Canal:* ${channel.channel_id}\n`;
+      msg += `*Descripción:* ${channel.channel_about}\n`;
+      msg += `*Fecha de Creación:* ${channel.channel_created}\n`;
+      msg += `*Imagen del Canal:* ${channel.channel_picture.medium.url}`;
+
+      await conn.sendMessage(m.chat, { text: msg }, { quoted: m });
+    } else {
+      throw 'No se encontraron resultados para el canal de YouTube proporcionado.';
+    }
+  } catch (error) {
+    throw `Ocurrió un error: ${error}`;
+  }
+};
+
+handler.help = ['youtubechannel'];
+handler.tags = ['dl'];
+handler.command = /^youtubechannel$/i;
+
+export default handler;
