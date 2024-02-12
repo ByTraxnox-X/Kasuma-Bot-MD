3094eefda6a2a6d3444bdef89011adc4dc9cefbf
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-12 13:14:20 -0500

    Actualizar gp-time.js

--- plugins/gp-time.js
+++ plugins/gp-time.js
@@ -3,9 +3,14 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
       global.dfail('admin', m, conn)
       throw false
     }
-  
+
+    if (!args[0]) {
+      m.reply(`No hay suficientes argumentos. Ejemplo de uso: ${usedPrefix + command} 6AM|7AM|7`);
+      throw false;
+    }
+
     let [horaApertura, horaCierre, dias] = args[0].split('|').map(e => e.trim());
-  
+
     if (!horaApertura || !horaCierre || !dias || isNaN(dias)) {
       m.reply(`Formato incorrecto. Ejemplo de uso: ${usedPrefix + command} 6AM|7AM|7`);
       throw false;
@@ -18,7 +23,7 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
     await conn.groupSettingUpdate(m.chat, 'not_announcement').then(async _ => {
       m.reply(`Grupo Abierto desde las ${horaApertura24} hasta las ${horaCierre24} durante ${dias} días.`);
     });
-  
+
     if (tiempoEstablecido > 0) {
       setTimeout(async () => {
         await conn.groupSettingUpdate(m.chat, 'not_announcement').then(async _ => {
@@ -27,15 +32,15 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
       }, tiempoEstablecido);
     }
   };
-  
+
   handler.help = ['programar 6AM|7AM|7'];
   handler.tags = ['group'];
   handler.command = /^programar$/i;
   handler.botAdmin = true;
   handler.group = true;
-  
+
   export default handler;
-  
+
   function obtenerFormato24Horas(hora) {
     let [horaNum, ampm] = hora.match(/(\d+)([APMapm]{2})/).slice(1);
     horaNum = parseInt(horaNum);
@@ -44,5 +49,5 @@ let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) =
     } else if (ampm.toUpperCase() === 'AM' && horaNum === 12) {
       horaNum = 0;
     }
-    return horaNum.toString().padStart(2, '0') + ':00';
+    return horaNum.toString().padStart(2, '0') + ':00';
   }
