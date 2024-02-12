f0ece8c4bc75aebd920b47e524c50f1ce0193dcb
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-12 14:37:56 -0500

    Actualizar gp-polling.js

--- plugins/gp-polling.js
+++ plugins/gp-polling.js
@@ -5,36 +5,29 @@ let handler = async (m, {
     usedPrefix,
     command
 }) => {
-    // Split the message text using the '|' character and slice the array to remove the first element.
-    let a = text.split("|").slice(1)
-    if (!a[1]) throw "Ejemplo de uso\n" + usedPrefix + command + " Como estan|bien|mal"
-    if (a[12]) throw "Ejemplo de uso\n" + usedPrefix + command + " Como estan|bien|mal"
-    // Check for duplicate options in the poll.
-    if (checkDuplicate(a)) throw "¡Opciones duplicadas en el mensaje!"
-    let cap = "*Encuesta hecha por:* " + m.name + "\n*Mensaje:* " + text.split("|")[0]
+    // Obtener la pregunta de la encuesta
+    let question = text.trim().split("quieren")[1].trim()
+    if (!question) {
+        throw "Por favor, haz una pregunta para la encuesta"
+    }
 
-   
+    // Crear el mensaje de la encuesta
     const pollMessage = {
-        name: cap,
-        values: a,
+        name: question,
+        values: ["Sí", "No"],
         multiselect: false,
         selectableCount: 1
     }
-  
+
+    // Enviar el mensaje de la encuesta al chat
     await conn.sendMessage(m.chat, {
         poll: pollMessage
     })
 }
 
-handler.help = ['encuesta <hola|como|xd>']
+handler.help = ['encuesta <pregunta>']
 handler.tags = ['group'] 
 handler.command = ['poll', 'encuesta', 'polling'] 
 handler.group = true
 
 export default handler
-
-// Function to check for duplicate elements in an array.
-function checkDuplicate(arr) {
-    return new Set(arr).size !== arr.length
-}
-
