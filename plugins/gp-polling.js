a535220548c6dd1675ca853b081506c058b9990f
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-12 20:38:25 -0500

    Actualizar gp-polling.js

--- plugins/gp-polling.js
+++ plugins/gp-polling.js
@@ -1,31 +1,20 @@
 let handler = async (m, {
-    conn,
-    text,
-    args,
-    command,
-    sender
+  conn,
+  text,
+  command,
+  sender
 }) => {
-    
-    let question = text.replace(/:.*/,'').trim()
-    if (!question) {
-        throw "Por favor, haz una pregunta para la encuesta"
-    }
-
-    const pollMessage = {
-        name: `Encuesta Realizada Por @${sender.split("@")[0]}:\n\n *${question}*`,
-        values: ["Sí", "No"],
-        multiselect: false,
-        selectableCount: 1
-    }
-
-    await conn.sendMessage(m.chat, {
-        poll: pollMessage
-    })
+  text = text ? text.trim() : ''
+  if (!text) return conn.reply(m.chat, 'Por favor, haz una pregunta para la encuesta', m)
+  const pollMessage = {
+    name: `Encuesta Realizada Por @${sender.split("@")[0]}:\n\n *${text}*`,
+    min: 1,
+    max: 1
+  }
+  await conn.sendMessage(m.chat, pollMessage, 'poll', pollMessage)
 }
-
 handler.help = ['encuesta <pregunta>']
-handler.tags = ['group'] 
-handler.command = ['poll', 'encuesta', 'polling'] 
+handler.tags = ['group']
+handler.command = ['poll', 'encuesta', 'polling']
 handler.group = true
-
 export default handler
