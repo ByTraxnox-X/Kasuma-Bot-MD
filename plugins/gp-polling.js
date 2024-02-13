1561873496a4428500e6b2bc44e7a3d730bbcef3
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-12 20:58:38 -0500

    Actualizar gp-polling.js

--- plugins/gp-polling.js
+++ plugins/gp-polling.js
@@ -1,22 +1,32 @@
 let handler = async (m, {
-  conn,
-  text,
-  command,
-  sender
+    conn,
+    text,
+    args,
+    command,
+    sender
 }) => {
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  text = text ? text.trim() : ''
-  if (!text) return conn.reply(m.chat, 'Por favor, haz una pregunta para la encuesta', m)
+
+    let question = text.replace(/:.*/,'').trim()
+    if (!question) {
+        throw "Por favor, haz una pregunta para la encuesta"
+    }
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let cap = "*Encuesta hecha por:* @${who.split('@')[0]}*\n*Mensaje:* ${text}"
-  const pollMessage = {
-    name: cap,
-    min: 1,
-    max: 1
-  }
-  await conn.sendMessage(m.chat, pollMessage, 'poll', pollMessage)
+    const pollMessage = {
+        name: cap,
+        values: ["Sí", "No"],
+        multiselect: false,
+        selectableCount: 1
+    }
+
+    await conn.sendMessage(m.chat, {
+        poll: pollMessage
+    })
 }
+
 handler.help = ['encuesta <pregunta>']
-handler.tags = ['group']
-handler.command = ['poll', 'encuesta', 'polling']
+handler.tags = ['group'] 
+handler.command = ['poll', 'encuesta', 'polling'] 
 handler.group = true
+
 export default handler
