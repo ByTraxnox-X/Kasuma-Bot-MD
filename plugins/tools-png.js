8d9bf801d469f144278d992f7ce72de0ca4dccf7
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-24 22:02:04 -0500

    Actualizar tools-png.js

--- plugins/tools-png.js
+++ plugins/tools-png.js
@@ -1,18 +1,25 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     const notImageMessage = `Envía una imagen y responde con:\n\n*${usedPrefix + command}*`;
-    if (!m.quoted) throw notImageMessage;
 
-    const q = m.quoted || m;
-    let mime = q.mimetype || '';
-    if (!mime.startsWith('image/')) throw notImageMessage;
+    if (!m.quoted && !m.mentionedJidList.length) throw notImageMessage;
 
-    let media = await q.download();
-    let out = Buffer.from(media);
-    await conn.sendFile(m.chat, out, 'out.png', '*Aquí tienes*', m);
+    let media = m.quoted ? await m.quoted.download() : await conn.getProfilePicture(m.mentionedJidList[0]);
+    if (!media) throw notImageMessage;
+
+    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0);
+
+    const options = ['pngdoc', 'pngimg'];
+    const selectedOption = options.includes(command) ? command : 'pngdoc';
+
+    if (selectedOption === 'pngdoc') {
+        await conn.sendFile(m.chat, out, 'out.png', '*Aquí tienes*', m);
+    } else if (selectedOption === 'pngimg') {
+        await conn.sendImage(m.chat, out, 'out.png', '*Aquí tienes*');
+    }
 };
 
-handler.help = ['png <imagen>'];
+handler.help = ['png <imagen>', 'png <mención>', 'pngdoc <imagen>', 'pngimg <imagen>'];
 handler.tags = ['tools'];
-handler.command = ['pngg', 'convpng', 'png'];
+handler.command = ['pngg', 'convpng', 'png', 'pngdoc', 'pngimg'];
 
 export default handler;
