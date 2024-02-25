ba9d352267888243664e11947cd0079f36ad08ad
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-24 22:24:05 -0500

    Actualizar tools-convdocumento.js

--- plugins/tools-convdocumento.js
+++ plugins/tools-convdocumento.js
@@ -4,27 +4,26 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let mime = (q.msg || q).mimetype || ''
 
     if (!m.quoted)
-        throw `Menciona una foto para convertir a documento PNG.`
+        throw `Responda a el Video o Audio que desea convertir a documento.`
 
     if (!text) throw `Ingrese el nombre que desea colocar al documento`
-    if (!/audio|video|image/.test(mime)) throw `Responda a la foto, video o audio que desea convertir a documento`
+    if (!/audio|video/.test(mime)) throw `Responda al video o audio que desea convertir a documento`
 
     let media = await q.download?.()
 
     if (!media) throw 'Error al descargar medio'
 
-    m.reply(`Convirtiendo, espera un momento...`)
+    m.reply(`${wait}`)
 
     if (/video/.test(mime)) {
+
         return conn.sendMessage(m.chat, { document: media, mimetype: 'video/mp4', fileName: `${text}.mp4` }, { quoted: m })
     } else if (/audio/.test(mime)) {
         return conn.sendMessage(m.chat, { document: media, mimetype: 'audio/mpeg', fileName: `${text}.mp3` }, { quoted: m })
-    } else if (/image/.test(mime)) {
-        return conn.sendMessage(m.chat, { document: media, mimetype: 'image/png', fileName: `${text}.png` }, { quoted: m })
     }
 }
 
-handler.help = ['convdocumento <audio/video/image>']
+handler.help = ['convdocumento <audio/video>']
 handler.tags = ['tools']
 handler.command = ['convdocumento']
 
