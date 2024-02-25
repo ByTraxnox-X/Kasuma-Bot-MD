79e19a77e581c8fb2e7df6b557a44b0cc140cfc2
ByTraxnox <86994318+ByTraxnox-X@users.noreply.github.com>
2024-02-24 22:14:12 -0500

    Actualizar tools-png.js

--- plugins/tools-png.js
+++ plugins/tools-png.js
@@ -1,38 +1,27 @@
 import { webp2png } from '../lib/webp2mp4.js';
 
-let handler = async (m, { conn, text }) => {
-    if (!text && (!m.quoted || m.quoted.mimetype.startsWith('image/'))) throw 'Envía una imagen y responde con:\n\n*.png*';
+let handler = async (m, { conn, usedPrefix, command }) => {
+    const notImageMessage = `Envía una imagen y responde con:\n\n*${usedPrefix + command}*`;
+    if (!m.quoted) throw notImageMessage;
 
-    let media;
-    if (text) {
-        const res = await conn.download({ url: text });
-        if (!res) throw 'No se pudo obtener la imagen correctamente.';
-        media = await res.buffer();
-    } else {
-        media = await m.quoted.download();
-    }
+    const q = m.quoted || m;
+    let mime = q.mimetype || '';
+    if (!mime.startsWith('image/')) throw notImageMessage;
 
-    if (!media.length) throw 'No se pudo obtener la imagen correctamente.';
-
+    let media = await q.download();
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0);
 
     if (!out.length) throw 'Error al convertir la imagen a PNG.';
 
-    let fileType = 'image/png';
+    let fileType = 'document';
     let fileName = 'out.png';
-
     const fileBuffer = Buffer.from(out);
 
-    if (m.text.toLowerCase().includes('doc')) {
-        fileType = 'document';
-        fileName = 'out.png';
-    }
-
-    await conn.send2Button(m.chat, `*Aquí tienes*`, '¿Enviarlo como Documento o Imagen?', 'Documento', `.pngdoc ${text || m.quoted ? 'enlace' : 'respuesta'}`, 'Imagen', `.pngimg ${text || m.quoted ? 'enlace' : 'respuesta'}`, m, { file: fileBuffer, type: fileType, mimetype: fileType, filename: fileName });
+    await conn.sendFile(m.chat, fileBuffer, fileName, '*Aquí tienes*', m);
 };
 
-handler.help = ['png <imagen>', 'png <enlace>'];
+handler.help = ['png <imagen>'];
 handler.tags = ['tools'];
-handler.command = ['png'];
+handler.command = ['pngimg', 'convpng', 'png'];
 
 export default handler;
