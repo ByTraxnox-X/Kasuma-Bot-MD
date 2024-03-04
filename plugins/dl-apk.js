4ccb9c37732629d50954c7998d0b2340776450f5
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-03-04 16:08:23 -0500

    KasumaBot

--- plugins/dl-apk.js
+++ plugins/dl-apk.js
@@ -9,21 +9,19 @@ const handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
 
     const apiUrl = `${apikasu}/api/dowloader/apk?apk=${text}&apikey=${apikeykasu}`;
     const response = await axios.get(apiUrl);
+    console.log('API Response:', response.data);
     const data = response.data.result;
 
     let responseText = `
-> Informacion
+> Información
 
 *Nombre:* ${data.name}\n\n
-*Ultima Actualizacion:* ${data.lastup}\n
-*Peso:* ${data.size}\n
-*Paquete:* ${data.package}\n`
-
+*Última Actualización:* ${data.lastup}\n
+*Tamaño:* ${data.size}\n
+*Paquete:* ${data.package}\n`;
     await conn.sendMessage(m.chat, { image: { url: data.icon }, caption: responseText }, { quoted: m });
-
     const apkResponse = await fetch(data.dllink);
     const apkBuffer = await apkResponse.buffer();
-
     await conn.sendFile(m.chat, apkBuffer, `${data.name}.apk`, null, m);
 
   } catch (error) {
@@ -39,4 +37,4 @@ handler.help = ['apk aplicacion'];
 handler.tags = ['dl'];
 handler.command = /^(apk|descargarapp|descargarapk)$/i;
 
-export default handler;
+export default handler;
