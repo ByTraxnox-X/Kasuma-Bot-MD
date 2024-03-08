21224cb56bec8fbd809a527f3617f3280c844f1a
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-03-08 12:19:35 -0500

    KasumaBot

--- plugins/gp-cmdnoexite.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn, isGroup, isCmd }) => {
-  if (!isGroup && isCmd) {
-    conn.sendMessage(m.chat, 'El comando aún no está disponible, ¡pruébalo en los próximos días!', 'text', { quoted: m });
-  }
-}
-
-export default handler
